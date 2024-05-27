a4aabbf4e1f9dba64866af58a4817958f984e287
thang.pham <vtp1201@gmail.com>
2024-05-28 00:43:49 +0700

    init

---
+++ src/databases/prisma/models/ActionLog.model.ts
@@ -0,0 +1,11 @@
+import { createModel } from 'schemix';
+
+import { ATTRIBUTE, COLUMN_NAME } from 'src/utilities/enums/actionLog';
+// import PostModel from './Post.model';
+// import UUIDMixin from '../mixins/UUID.mixin';
+
+export default createModel('actionLog', (ActionLogModel) => {
+  ActionLogModel.int(ATTRIBUTE.id, {
+    map: COLUMN_NAME.id,
+  });
+});
---
+++ src/utilities/enums/actionLog.ts
@@ -0,0 +1,17 @@
+export enum ATTRIBUTE {
+  id = 'id',
+  userId = 'user_id',
+  description = 'description',
+  type = 'type',
+  createdTime = 'created_time',
+  isDeleted = 'is_deleted',
+}
+
+export enum COLUMN_NAME {
+  id = 'id',
+  userId = 'user_id',
+  description = 'description',
+  type = 'type',
+  createdTime = 'created_time',
+  isDeleted = 'is_deleted',
+}
