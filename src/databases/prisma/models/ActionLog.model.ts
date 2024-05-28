31bbdba95260a22d7fcefc4ccffd8b6d97e9f798
Pham Viet Thang <thangpham@gleadsvn.local>
2024-05-28 17:52:38 +0700

    module alias

--- src/databases/prisma/models/ActionLog.model.ts
+++ src/databases/prisma/models/ActionLog.model.ts
@@ -1,6 +1,7 @@
 import { createModel } from 'schemix';
 
 import { ATTRIBUTE, COLUMN_NAME } from 'src/utilities/enums/actionLog';
+import { MODEL_NAME } from '@utilities';
 // import PostModel from './Post.model';
 // import UUIDMixin from '../mixins/UUID.mixin';
 
