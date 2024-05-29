4e3fe1c166129023a9ff618bbe29e3667e9575b2
Pham Viet Thang <thangpham@gleadsvn.local>
2024-05-29 09:05:38 +0700

    init webpack

--- src/databases/prisma/models/ActionLog.model.ts
+++ src/databases/prisma/models/ActionLog.model.ts
@@ -1,11 +1,11 @@
 import { createModel } from 'schemix';
 
 import { ATTRIBUTE, COLUMN_NAME } from 'src/utilities/enums/actionLog';
-import { MODEL_NAME } from '@utilities';
+import { MODEL_NAME } from 'utilities';
 // import PostModel from './Post.model';
 // import UUIDMixin from '../mixins/UUID.mixin';
 
-export default createModel('actionLog', (ActionLogModel) => {
+export default createModel(MODEL_NAME.ACTION_LOG, (ActionLogModel) => {
   ActionLogModel.int(ATTRIBUTE.id, {
     map: COLUMN_NAME.id,
   });
