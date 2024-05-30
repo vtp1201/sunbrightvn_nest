fc43d4006320be9b3355c019149b1fc695eef405
Pham Viet Thang <thangpham@gleadsvn.local>
2024-05-30 14:06:00 +0700

    init agency and agent

---
+++ src/databases/prisma/utils/enums/Agency.ts
@@ -0,0 +1,28 @@
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
+
+export enum ATTRIBUTE {
+  id = 'id',
+  name = 'name',
+  createdTime = ATTRIBUTE_DEFAULT.createdTime,
+  updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
+  isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
+  deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
+}
+
+export enum COLUMN {
+  id = 'id',
+  name = 'name',
+  createdTime = COLUMN_DEFAULT.createdTime,
+  updatedTime = COLUMN_DEFAULT.updatedTime,
+  isDeleted = COLUMN_DEFAULT.isDeleted,
+  deletedTime = COLUMN_DEFAULT.deletedTime,
+}
+
+export enum RELATION {
+  agents = RELATION_DEFAULT.agents,
+}
+
+export default {
+  ATTRIBUTE,
+  RELATION,
+};
---
+++ src/databases/prisma/utils/enums/Agent.ts
@@ -0,0 +1,47 @@
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
+
+export enum ATTRIBUTE {
+  id = 'id',
+  name = 'name',
+  email = 'email',
+  countryId = 'countryId',
+  agencyId = 'agencyId',
+  agentTypeId = 'agentTypeId',
+  bankId = 'bankId',
+  isHasNominee = 'isHasNominee',
+  createdTime = ATTRIBUTE_DEFAULT.createdTime,
+  updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
+  isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
+  deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
+}
+
+export enum COLUMN {
+  id = 'id',
+  name = 'name',
+  email = 'email',
+  countryId = 'country_id',
+  agencyId = 'agency_id',
+  agentTypeId = 'agent_type_id',
+  bankId = 'bank_id',
+  isHasNominee = 'is_has_nominee',
+  createdTime = COLUMN_DEFAULT.createdTime,
+  updatedTime = COLUMN_DEFAULT.updatedTime,
+  isDeleted = COLUMN_DEFAULT.isDeleted,
+  deletedTime = COLUMN_DEFAULT.deletedTime,
+}
+
+export enum RELATION {
+  agency = RELATION_DEFAULT.agency,
+  bank = RELATION_DEFAULT.bank,
+  country = RELATION_DEFAULT.country,
+  agentType = RELATION_DEFAULT.agentType,
+  emails = RELATION_DEFAULT.emails,
+  processLogs = RELATION_DEFAULT.processLogs,
+  taskHasAgents = RELATION_DEFAULT.taskHasAgents,
+  tokens = RELATION_DEFAULT.tokens,
+}
+
+export default {
+  ATTRIBUTE,
+  RELATION,
+};
