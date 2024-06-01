28c8a623b9121770565847873480e74e3823d27e
thang.pham <vtp1201@gmail.com>
2024-06-01 22:04:30 +0700

    init model CompanyEventType

---
+++ src/databases/prisma/models/CompanyEventType.ts
@@ -0,0 +1,35 @@
+import { createModel } from 'schemix';
+
+import { MODEL_NAME, TABLE_NAME, RAW_STRING } from '../utils';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/CompanyEventType';
+import { createdTime, deleted, updatedTime } from '../mixins';
+
+export default createModel(
+  MODEL_NAME.COMPANY_EVENT_TYPE,
+  (CompanyEventTypeModel) => {
+    CompanyEventTypeModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .string(ATTRIBUTE.name, {
+        map: COLUMN.name,
+        raw: RAW_STRING.TEXT,
+      })
+      .string(ATTRIBUTE.description, {
+        map: COLUMN.description,
+        raw: RAW_STRING.TEXT,
+        optional: true,
+      })
+
+      // dateTime marks
+      .mixin(createdTime)
+      .mixin(updatedTime)
+      .mixin(deleted)
+
+      // table name
+      .map(TABLE_NAME.COMPANY_EVENT_TYPE);
+  },
+);
---
+++ src/databases/prisma/utils/enums/CompanyEventType.ts
@@ -0,0 +1,30 @@
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
+
+export enum ATTRIBUTE {
+  id = 'id',
+  name = 'name',
+  description = 'description',
+  createdTime = ATTRIBUTE_DEFAULT.createdTime,
+  updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
+  isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
+  deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
+}
+
+export enum COLUMN {
+  id = 'id',
+  name = 'name',
+  description = 'description',
+  createdTime = COLUMN_DEFAULT.createdTime,
+  updatedTime = COLUMN_DEFAULT.updatedTime,
+  isDeleted = COLUMN_DEFAULT.isDeleted,
+  deletedTime = COLUMN_DEFAULT.deletedTime,
+}
+
+export enum RELATION {
+  companyEvents = RELATION_DEFAULT.companyEvents,
+}
+
+export default {
+  ATTRIBUTE,
+  RELATION,
+};
