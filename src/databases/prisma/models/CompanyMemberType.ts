f1f581d10b2a6ec718842400f22d42b25faa1c8c
thang.pham <vtp1201@gmail.com>
2024-06-04 00:53:37 +0700

    init model CompanyMemberType

---
+++ src/databases/prisma/models/CompanyMemberType.ts
@@ -0,0 +1,43 @@
+import { createModel } from 'schemix';
+
+import { MODEL_NAME, TABLE_NAME, RAW_STRING } from '../utils';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/CompanyMemberType';
+import { createdTime, deleted, updatedTime } from '../mixins';
+
+export default createModel(
+  MODEL_NAME.COMPANY_MEMBER_TYPE,
+  (CompanyMemberTypeModel) => {
+    CompanyMemberTypeModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .string(ATTRIBUTE.name, {
+        map: COLUMN.name,
+        raw: RAW_STRING.LENGTH_45,
+      })
+      .string(ATTRIBUTE.description, {
+        map: COLUMN.description,
+        raw: RAW_STRING.TEXT,
+        optional: true,
+      })
+      .int(ATTRIBUTE.parentId, {
+        map: COLUMN.parentId,
+        optional: true,
+      })
+      .boolean(ATTRIBUTE.isRequired, {
+        map: COLUMN.isRequired,
+        default: false,
+      })
+
+      // dateTime marks
+      .mixin(createdTime)
+      .mixin(updatedTime)
+      .mixin(deleted)
+
+      // table name
+      .map(TABLE_NAME.COMPANY_MEMBER_TYPE);
+  },
+);
