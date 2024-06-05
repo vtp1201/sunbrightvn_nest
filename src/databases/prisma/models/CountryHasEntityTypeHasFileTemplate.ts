6734fb629698de84b04936925d9cd13db72ff4ef
thang.pham <vtp1201@gmail.com>
2024-06-05 23:06:32 +0700

    init model CountryHasEntityTypeHasFileTemplate

---
+++ src/databases/prisma/models/CountryHasEntityTypeHasFileTemplate.ts
@@ -0,0 +1,28 @@
+import { createModel } from 'schemix';
+
+import { MODEL_NAME, TABLE_NAME } from '../utils';
+import {
+  ATTRIBUTE,
+  COLUMN,
+} from '../utils/enums/CountryHasEntityTypeHasFileTemplate';
+
+export default createModel(
+  MODEL_NAME.COUNTRY_HAS_ENTITY_TYPE_HAS_FILE_TEMPLATE,
+  (CountryHasEntityTypeHasFileTemplateModel) => {
+    CountryHasEntityTypeHasFileTemplateModel.int(
+      ATTRIBUTE.countryHasEntityTypeId,
+      {
+        map: COLUMN.countryHasEntityTypeId,
+      },
+    )
+      .int(ATTRIBUTE.fileTemplateId, {
+        map: COLUMN.fileTemplateId,
+      })
+      .id({
+        fields: [ATTRIBUTE.countryHasEntityTypeId, ATTRIBUTE.fileTemplateId],
+      })
+
+      // table name
+      .map(TABLE_NAME.COUNTRY_HAS_ENTITY_TYPE_HAS_FILE_TEMPLATE);
+  },
+);
