0bb91ad6d933f920200b52028f79b12024f04415
thang.pham <vtp1201@gmail.com>
2024-06-05 23:00:10 +0700

    init model Country

---
+++ src/databases/prisma/models/Country.ts
@@ -0,0 +1,56 @@
+import { createModel } from 'schemix';
+
+import { MODEL_NAME, TABLE_NAME, RAW_STRING } from '../utils';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/Country';
+import { deleted } from '../mixins';
+
+export default createModel(MODEL_NAME.COUNTRY, (CountryModel) => {
+  CountryModel.int(ATTRIBUTE.id, {
+    id: true,
+    map: COLUMN.id,
+    default: {
+      autoincrement: true,
+    },
+  })
+    .string(ATTRIBUTE.name, {
+      map: COLUMN.name,
+      raw: RAW_STRING.LENGTH_45,
+    })
+    .string(ATTRIBUTE.guid, {
+      map: COLUMN.guid,
+      raw: RAW_STRING.LENGTH_100,
+      unique: true,
+    })
+    .string(ATTRIBUTE.countryCode, {
+      map: COLUMN.countryCode,
+      raw: RAW_STRING.LENGTH_45,
+      optional: true,
+    })
+    .string(ATTRIBUTE.phoneCode, {
+      map: COLUMN.phoneCode,
+      raw: RAW_STRING.LENGTH_45,
+      optional: true,
+    })
+    .string(ATTRIBUTE.subName, {
+      map: COLUMN.subName,
+      raw: RAW_STRING.LENGTH_255,
+    })
+    .boolean(ATTRIBUTE.isRestricted, {
+      map: COLUMN.isRestricted,
+      default: false,
+    })
+    .boolean(ATTRIBUTE.isCountry, {
+      map: COLUMN.isCountry,
+      default: false,
+    })
+    .int(ATTRIBUTE.continentId, {
+      map: COLUMN.continentId,
+      optional: true,
+    })
+
+    // dateTime marks
+    .mixin(deleted)
+
+    // table name
+    .map(TABLE_NAME.COUNTRY);
+});
