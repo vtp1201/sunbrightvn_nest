135d9481d25d4efc614d4e92ebba522bd7f9b721
thang.pham <vtp1201@gmail.com>
2024-06-06 23:17:46 +0700

    init model CustomerHasSurveyChoice

--- src/databases/prisma/models/CustomerHasSurveyChoice.ts
+++ src/databases/prisma/models/CustomerHasSurveyChoice.ts
@@ -1,22 +1,34 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
-import { ATTRIBUTE, COLUMN } from '../utils/enums/CustomerHasBank';
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/CustomerHasSurveyChoice';
+import { createdTime, deleted, updatedTime } from '../mixins';
 
 export default createModel(
-  MODEL_NAME.CUSTOMER_HAS_BANK,
-  (CustomerHasBankModel) => {
-    CustomerHasBankModel.int(ATTRIBUTE.customerId, {
+  MODEL_NAME.CUSTOMER_HAS_SURVEY_CHOICE,
+  (CustomerHasSurveyChoiceModel) => {
+    CustomerHasSurveyChoiceModel.int(ATTRIBUTE.customerId, {
       map: COLUMN.customerId,
     })
-      .int(ATTRIBUTE.bankId, {
-        map: COLUMN.bankId,
+      .int(ATTRIBUTE.surveyChoiceId, {
+        map: COLUMN.surveyChoiceId,
       })
+      .string(ATTRIBUTE.surveyContent, {
+        map: COLUMN.surveyContent,
+        raw: RAW_STRING.LENGTH_255,
+        optional: true,
+      })
+
+      // dateTime marks
+      .mixin(createdTime)
+      .mixin(updatedTime)
+      .mixin(deleted)
+
       .id({
-        fields: [ATTRIBUTE.customerId, ATTRIBUTE.bankId],
+        fields: [ATTRIBUTE.customerId, ATTRIBUTE.surveyChoiceId],
       })
 
       // table name
-      .map(TABLE_NAME.CUSTOMER_HAS_BANK);
+      .map(TABLE_NAME.CUSTOMER_HAS_SURVEY_CHOICE);
   },
 );
