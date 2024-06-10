63c5d4d33297f6cb22ab5661dcc280923ff440a1
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-10 13:17:34 +0700

    init

---
+++ src/databases/prisma/models/Department.ts
@@ -0,0 +1,45 @@
+import { createModel } from 'schemix';
+
+import { MODEL_NAME, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/Department';
+import { createdTime, deleted, updatedTime } from '../mixins';
+
+export default createModel(MODEL_NAME.DEPARTMENT, (DepartmentModel) => {
+  const initCreatedTime = createdTime({
+    attribute: ATTRIBUTE.createdTime,
+    column: COLUMN.createdTime,
+  });
+  const initUpdatedTime = updatedTime({
+    attribute: ATTRIBUTE.updatedTime,
+    column: COLUMN.updatedTime,
+  });
+  const initDeleted = deleted(
+    {
+      attribute: ATTRIBUTE.deletedTime,
+      column: COLUMN.deletedTime,
+    },
+    {
+      attribute: ATTRIBUTE.isDeleted,
+      column: COLUMN.isDeleted,
+    },
+  );
+
+  // defined Model
+  process.nextTick(() => {
+    DepartmentModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+
+      // dateTime marks
+      .mixin(initCreatedTime)
+      .mixin(initUpdatedTime)
+      .mixin(initDeleted)
+
+      // table name
+      .map(TABLE_NAME.DEPARTMENT);
+  });
+});
---
+++ src/databases/prisma/models/EmailTemplateHasReceiver.ts
@@ -0,0 +1,48 @@
+import { createModel } from 'schemix';
+
+import { MODEL_NAME, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/EmailTemplateHasReceiver';
+import { createdTime, deleted, updatedTime } from '../mixins';
+
+export default createModel(
+  MODEL_NAME.EMAIL_TEMPLATE_HAS_RECEIVER,
+  (EmailTemplateHasReceiverModel) => {
+    const initCreatedTime = createdTime({
+      attribute: ATTRIBUTE.createdTime,
+      column: COLUMN.createdTime,
+    });
+    const initUpdatedTime = updatedTime({
+      attribute: ATTRIBUTE.updatedTime,
+      column: COLUMN.updatedTime,
+    });
+    const initDeleted = deleted(
+      {
+        attribute: ATTRIBUTE.deletedTime,
+        column: COLUMN.deletedTime,
+      },
+      {
+        attribute: ATTRIBUTE.isDeleted,
+        column: COLUMN.isDeleted,
+      },
+    );
+
+    // defined Model
+    process.nextTick(() => {
+      EmailTemplateHasReceiverModel.int(ATTRIBUTE.id, {
+        id: true,
+        map: COLUMN.id,
+        default: {
+          autoincrement: true,
+        },
+      })
+
+        // dateTime marks
+        .mixin(initCreatedTime)
+        .mixin(initUpdatedTime)
+        .mixin(initDeleted)
+
+        // table name
+        .map(TABLE_NAME.EMAIL_TEMPLATE_HAS_RECEIVER);
+    });
+  },
+);
