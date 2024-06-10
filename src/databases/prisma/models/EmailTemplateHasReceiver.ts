f32c610c54a66d7b3c44090d48169e5f9b09ea07
thang.pham <vtp1201@gmail.com>
2024-06-10 20:46:18 +0700

    init model EmailTemplateHasReceiver

--- src/databases/prisma/models/EmailTemplateHasReceiver.ts
+++ src/databases/prisma/models/EmailTemplateHasReceiver.ts
@@ -1,7 +1,11 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
-import { ATTRIBUTE, COLUMN } from '../utils/enums/EmailTemplateHasReceiver';
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
+import {
+  ATTRIBUTE,
+  COLUMN,
+  INDEX,
+} from '../utils/enums/EmailTemplateHasReceiver';
 import { createdTime, deleted, updatedTime } from '../mixins';
 
 export default createModel(
@@ -35,11 +39,53 @@ export default createModel(
           autoincrement: true,
         },
       })
+        .int(ATTRIBUTE.emailTemplateId, {
+          map: COLUMN.emailTemplateId,
+        })
+        .string(ATTRIBUTE.email, {
+          map: COLUMN.email,
+          optional: true,
+          raw: RAW_STRING.LENGTH_100,
+        })
+        .int(ATTRIBUTE.userId, {
+          map: COLUMN.userId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.roleId, {
+          map: COLUMN.roleId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.groupId, {
+          map: COLUMN.groupId,
+          optional: true,
+        })
+        .boolean(ATTRIBUTE.isMainReceiver, {
+          map: COLUMN.isMainReceiver,
+          default: false,
+        })
+        .boolean(ATTRIBUTE.isCcReceiver, {
+          map: COLUMN.isCcReceiver,
+          default: false,
+        })
+        .boolean(ATTRIBUTE.isBccReceiver, {
+          map: COLUMN.isBccReceiver,
+          default: false,
+        })
+        .boolean(ATTRIBUTE.isExcluded, {
+          map: COLUMN.isExcluded,
+          default: false,
+        })
 
         // dateTime marks
         .mixin(initCreatedTime)
         .mixin(initUpdatedTime)
         .mixin(initDeleted)
+
+        // indexes
+        .raw(INDEX.emailTemplateId)
+        .raw(INDEX.groupId)
+        .raw(INDEX.roleId)
+        .raw(INDEX.userId)
 
         // table name
         .map(TABLE_NAME.EMAIL_TEMPLATE_HAS_RECEIVER);
