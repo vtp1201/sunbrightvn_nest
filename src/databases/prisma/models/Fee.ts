42ab4eb0d14a53c8e36e5b721d3849d11b637be1
thang.pham <vtp1201@gmail.com>
2024-06-11 21:53:16 +0700

    init FeeModel

--- src/databases/prisma/models/Fee.ts
+++ src/databases/prisma/models/Fee.ts
@@ -1,49 +1,61 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
-import { ATTRIBUTE, COLUMN } from '../utils/enums/Fee';
+import { MODEL_NAME, RAW_NUMBER, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/Fee';
 import { createdTime, deleted, updatedTime } from '../mixins';
 
-export default createModel(
-  MODEL_NAME.FEE,
-  (FeeModel) => {
-    const initCreatedTime = createdTime({
-      attribute: ATTRIBUTE.createdTime,
-      column: COLUMN.createdTime,
-    });
-    const initUpdatedTime = updatedTime({
-      attribute: ATTRIBUTE.updatedTime,
-      column: COLUMN.updatedTime,
-    });
-    const initDeleted = deleted(
-      {
-        attribute: ATTRIBUTE.deletedTime,
-        column: COLUMN.deletedTime,
-      },
-      {
-        attribute: ATTRIBUTE.isDeleted,
-        column: COLUMN.isDeleted,
-      },
-    );
+export default createModel(MODEL_NAME.FEE, (FeeModel) => {
+  const initCreatedTime = createdTime({
+    attribute: ATTRIBUTE.createdTime,
+    column: COLUMN.createdTime,
+  });
+  const initUpdatedTime = updatedTime({
+    attribute: ATTRIBUTE.updatedTime,
+    column: COLUMN.updatedTime,
+  });
+  const initDeleted = deleted(
+    {
+      attribute: ATTRIBUTE.deletedTime,
+      column: COLUMN.deletedTime,
+    },
+    {
+      attribute: ATTRIBUTE.isDeleted,
+      column: COLUMN.isDeleted,
+    },
+  );
 
-    // defined Model
-    process.nextTick(() => {
-      FeeModel.int(ATTRIBUTE.id, {
-        id: true,
-        map: COLUMN.id,
-        default: {
-          autoincrement: true,
-        },
+  // defined Model
+  process.nextTick(() => {
+    FeeModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .int(ATTRIBUTE.serviceId, {
+        map: COLUMN.serviceId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.feeTypeId, {
+        map: COLUMN.feeTypeId,
+      })
+      .float(ATTRIBUTE.value, {
+        map: COLUMN.value,
+        optional: true,
+        raw: RAW_NUMBER.FLOAT,
       })
 
-        // dateTime marks
-        .mixin(initCreatedTime)
-        .mixin(initUpdatedTime)
-        .mixin(initDeleted)
+      // dateTime marks
+      .mixin(initCreatedTime)
+      .mixin(initUpdatedTime)
+      .mixin(initDeleted)
 
-        // table name
-        .map(TABLE_NAME.FEE);
-    });
-  },
-);
+      // indexes
+      .raw(INDEX.feeTypeId)
+      .raw(INDEX.serviceId)
 
+      // table name
+      .map(TABLE_NAME.FEE);
+  });
+});
