d065060ee8e3d2c27a9b630d563351884f338d04
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-12 11:39:13 +0700

    init GrantSchemeModel

--- src/databases/prisma/models/GrantScheme.ts
+++ src/databases/prisma/models/GrantScheme.ts
@@ -1,49 +1,86 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
-import { ATTRIBUTE, COLUMN } from '../utils/enums/GrantScheme';
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/GrantScheme';
 import { createdTime, deleted, updatedTime } from '../mixins';
 
-export default createModel(
-  MODEL_NAME.GRANT_SCHEME,
-  (GrantSchemeModel) => {
-    const initCreatedTime = createdTime({
-      attribute: ATTRIBUTE.createdTime,
-      column: COLUMN.createdTime,
-    });
-    const initUpdatedTime = updatedTime({
-      attribute: ATTRIBUTE.updatedTime,
-      column: COLUMN.updatedTime,
-    });
-    const initDeleted = deleted(
-      {
-        attribute: ATTRIBUTE.deletedTime,
-        column: COLUMN.deletedTime,
-      },
-      {
-        attribute: ATTRIBUTE.isDeleted,
-        column: COLUMN.isDeleted,
-      },
-    );
+export default createModel(MODEL_NAME.GRANT_SCHEME, (GrantSchemeModel) => {
+  const initCreatedTime = createdTime({
+    attribute: ATTRIBUTE.createdTime,
+    column: COLUMN.createdTime,
+  });
+  const initUpdatedTime = updatedTime({
+    attribute: ATTRIBUTE.updatedTime,
+    column: COLUMN.updatedTime,
+  });
+  const initDeleted = deleted(
+    {
+      attribute: ATTRIBUTE.deletedTime,
+      column: COLUMN.deletedTime,
+    },
+    {
+      attribute: ATTRIBUTE.isDeleted,
+      column: COLUMN.isDeleted,
+    },
+  );
 
-    // defined Model
-    process.nextTick(() => {
-      GrantSchemeModel.int(ATTRIBUTE.id, {
-        id: true,
-        map: COLUMN.id,
-        default: {
-          autoincrement: true,
-        },
+  // defined Model
+  process.nextTick(() => {
+    GrantSchemeModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .string(ATTRIBUTE.name, {
+        map: COLUMN.name,
+        raw: RAW_STRING.LENGTH_100,
+      })
+      .string(ATTRIBUTE.description, {
+        map: COLUMN.description,
+        optional: true,
+        raw: RAW_STRING.TEXT,
+      })
+      .int(ATTRIBUTE.countryId, {
+        map: COLUMN.countryId,
+        optional: true,
+      })
+      .string(ATTRIBUTE.keyFacts, {
+        map: COLUMN.keyFacts,
+        optional: true,
+        raw: RAW_STRING.TEXT,
+      })
+      .string(ATTRIBUTE.introduction, {
+        map: COLUMN.introduction,
+        optional: true,
+        raw: RAW_STRING.TEXT,
+      })
+      .string(ATTRIBUTE.benefits, {
+        map: COLUMN.benefits,
+        optional: true,
+        raw: RAW_STRING.TEXT,
+      })
+      .string(ATTRIBUTE.eligibility, {
+        map: COLUMN.eligibility,
+        optional: true,
+        raw: RAW_STRING.TEXT,
+      })
+      .string(ATTRIBUTE.howToApply, {
+        map: COLUMN.howToApply,
+        optional: true,
+        raw: RAW_STRING.TEXT,
       })
 
-        // dateTime marks
-        .mixin(initCreatedTime)
-        .mixin(initUpdatedTime)
-        .mixin(initDeleted)
+      // dateTime marks
+      .mixin(initCreatedTime)
+      .mixin(initUpdatedTime)
+      .mixin(initDeleted)
 
-        // table name
-        .map(TABLE_NAME.GRANT_SCHEME);
-    });
-  },
-);
+      // indexes
+      .raw(INDEX.countryId)
 
+      // table name
+      .map(TABLE_NAME.GRANT_SCHEME);
+  });
+});
--- src/databases/prisma/utils/enums/GrantScheme.ts
+++ src/databases/prisma/utils/enums/GrantScheme.ts
@@ -1,27 +1,47 @@
-import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT } from './default';
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
 
 export enum ATTRIBUTE {
   id = 'id',
-  createdTime = ATTRIBUTE_DEFAULT.createdTime,
+  name = 'name',
+  description = 'description',
+  countryId = 'countryId',
+  keyFacts = 'keyFacts',
+  introduction = 'introduction',
+  benefits = 'benefits',
+  eligibility = 'eligibility',
+  howToApply = 'howToApply',
   updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
+  createdTime = ATTRIBUTE_DEFAULT.createdTime,
   deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
   isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
 }
 
 export enum COLUMN {
   id = 'id',
-  createdTime = COLUMN_DEFAULT.createdTime,
+  name = 'name',
+  description = 'description',
+  countryId = 'country_id',
+  keyFacts = 'key_facts',
+  introduction = 'introduction',
+  benefits = 'benefits',
+  eligibility = 'eligibility',
+  howToApply = 'how_to_apply',
   updatedTime = COLUMN_DEFAULT.updatedTime,
+  createdTime = COLUMN_DEFAULT.createdTime,
   deletedTime = COLUMN_DEFAULT.deletedTime,
   isDeleted = COLUMN_DEFAULT.isDeleted,
 }
 
 export enum RELATION {
+  country = RELATION_DEFAULT.country,
+  grantSchemeHasGrantTypeAttributes = RELATION_DEFAULT.grantSchemeHasGrantTypeAttributes,
+}
 
+export enum INDEX {
+  countryId = `@@index([${ATTRIBUTE.countryId}], map: "fk_grant_scheme_country_idx")`,
 }
 
 export default {
   ATTRIBUTE,
   RELATION,
 };
-
