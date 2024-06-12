c4e023a463d8e375656bc806ef75ee609dc529ed
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-12 11:55:24 +0700

    init GrantSchemeHasGrantTypeAttributeModel

--- src/databases/prisma/models/GrantSchemeHasGrantTypeAttribute.ts
+++ src/databases/prisma/models/GrantSchemeHasGrantTypeAttribute.ts
@@ -1,7 +1,11 @@
 import { createModel } from 'schemix';
 
 import { MODEL_NAME, TABLE_NAME } from '../utils';
-import { ATTRIBUTE, COLUMN } from '../utils/enums/GrantSchemeHasGrantTypeAttribute';
+import {
+  ATTRIBUTE,
+  COLUMN,
+  INDEX,
+} from '../utils/enums/GrantSchemeHasGrantTypeAttribute';
 import { createdTime, deleted, updatedTime } from '../mixins';
 
 export default createModel(
@@ -35,15 +39,65 @@ export default createModel(
           autoincrement: true,
         },
       })
+        .int(ATTRIBUTE.grantSchemeId, {
+          map: COLUMN.grantSchemeId,
+        })
+        .int(ATTRIBUTE.needsBasedId, {
+          map: COLUMN.needsBasedId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.industryId, {
+          map: COLUMN.industryId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.businessStageId, {
+          map: COLUMN.businessStageId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.yearsOperationId, {
+          map: COLUMN.yearsOperationId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.businessSizeId, {
+          map: COLUMN.businessSizeId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.localShareholdingId, {
+          map: COLUMN.localShareholdingId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.numberEmployeesId, {
+          map: COLUMN.numberEmployeesId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.typeId, {
+          map: COLUMN.typeId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.agencyId, {
+          map: COLUMN.agencyId,
+          optional: true,
+        })
 
         // dateTime marks
         .mixin(initCreatedTime)
         .mixin(initUpdatedTime)
         .mixin(initDeleted)
 
+        // indexes
+        .raw(INDEX.agencyId)
+        .raw(INDEX.businessSizeId)
+        .raw(INDEX.businessStageId)
+        .raw(INDEX.grantSchemeId)
+        .raw(INDEX.industryId)
+        .raw(INDEX.localShareholdingId)
+        .raw(INDEX.needsBasedId)
+        .raw(INDEX.numberEmployeesId)
+        .raw(INDEX.typeId)
+        .raw(INDEX.yearsOperationId)
+
         // table name
         .map(TABLE_NAME.GRANT_SCHEME_HAS_GRANT_TYPE_ATTRIBUTE);
     });
   },
 );
-
--- src/databases/prisma/utils/enums/GrantSchemeHasGrantTypeAttribute.ts
+++ src/databases/prisma/utils/enums/GrantSchemeHasGrantTypeAttribute.ts
@@ -1,7 +1,17 @@
-import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT } from './default';
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
 
 export enum ATTRIBUTE {
   id = 'id',
+  grantSchemeId = 'grantSchemeId',
+  needsBasedId = 'needsBasedId',
+  industryId = 'industryId',
+  businessStageId = 'businessStageId',
+  yearsOperationId = 'yearsOperationId',
+  businessSizeId = 'businessSizeId',
+  localShareholdingId = 'localShareholdingId',
+  numberEmployeesId = 'numberEmployeesId',
+  typeId = 'typeId',
+  agencyId = 'agencyId',
   createdTime = ATTRIBUTE_DEFAULT.createdTime,
   updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
   deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
@@ -10,18 +20,48 @@ export enum ATTRIBUTE {
 
 export enum COLUMN {
   id = 'id',
+  grantSchemeId = 'grant_scheme_id',
+  needsBasedId = 'needs_based_id',
+  industryId = 'industry_id',
+  businessStageId = 'business_stage_id',
+  yearsOperationId = 'years_operation_id',
+  businessSizeId = 'business_size_id',
+  localShareholdingId = 'local_shareholding_id',
+  numberEmployeesId = 'number_employees_id',
+  typeId = 'type_id',
+  agencyId = 'agency_id',
   createdTime = COLUMN_DEFAULT.createdTime,
   updatedTime = COLUMN_DEFAULT.updatedTime,
   deletedTime = COLUMN_DEFAULT.deletedTime,
   isDeleted = COLUMN_DEFAULT.isDeleted,
 }
 
-export enum RELATION {
+export enum RELATION {}
+// grantScheme = RELATION_DEFAULT.grantScheme,
+// grantTypeAttributeAgency = RELATION_DEFAULT.grantTypeAttributeAgency,
+// grantTypeAttributeBusinessSize = RELATION_DEFAULT.grantTypeAttributeBusinessSize,
+// grantTypeAttributeBusinessStage = RELATION_DEFAULT.grantTypeAttributeBusinessStage,
+// grantTypeAttributeIndustry = RELATION_DEFAULT.grantTypeAttributeIndustry,
+// grantTypeAttributeLocalShareholding = RELATION_DEFAULT.grantTypeAttributeLocalShareholding,
+// grantTypeAttributeNeedsBased = RELATION_DEFAULT.grantTypeAttributeNeedsBased,
+// grantTypeAttributeNumberEmployees = RELATION_DEFAULT.grantTypeAttributeNumberEmployees,
+// grantTypeAttributeType = RELATION_DEFAULT.grantTypeAttributeType,
+// grantTypeAttributeYearsOperation = RELATION_DEFAULT.grantTypeAttributeYearsOperation,
 
+export enum INDEX {
+  agencyId = `@@index([${ATTRIBUTE.agencyId}], map: "fk_grant_scheme_has_grant_type_attribute_agency_idx")`,
+  businessSizeId = `@@index([${ATTRIBUTE.businessSizeId}], map: "fk_grant_scheme_has_grant_type_attribute_business_size_idx")`,
+  businessStageId = `@@index([${ATTRIBUTE.businessStageId}], map: "fk_grant_scheme_has_grant_type_attribute_business_stage_idx")`,
+  grantSchemeId = `@@index([${ATTRIBUTE.grantSchemeId}], map: "fk_grant_scheme_has_grant_type_attribute_grant_scheme_idx")`,
+  industryId = `@@index([${ATTRIBUTE.industryId}], map: "fk_grant_scheme_has_grant_type_attribute_industry_idx")`,
+  localShareholdingId = `@@index([${ATTRIBUTE.localShareholdingId}], map: "fk_grant_scheme_has_grant_type_attribute_local_shareholding_idx")`,
+  needsBasedId = `@@index([${ATTRIBUTE.needsBasedId}], map: "fk_grant_scheme_has_grant_type_attribute_needs_base_idx")`,
+  numberEmployeesId = `@@index([${ATTRIBUTE.numberEmployeesId}], map: "fk_grant_scheme_has_grant_type_attribute_number_employees_idx")`,
+  typeId = `@@index([${ATTRIBUTE.typeId}], map: "fk_grant_scheme_has_grant_type_attribute_type_idx")`,
+  yearsOperationId = `@@index([${ATTRIBUTE.yearsOperationId}], map: "fk_grant_scheme_has_grant_type_attribute_years_operation_idx")`,
 }
 
 export default {
   ATTRIBUTE,
   RELATION,
 };
-
