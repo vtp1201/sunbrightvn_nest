b69197fe1dd1f92a84d5447c844fcdbcc18f8ca9
thang.pham <vtp1201@gmail.com>
2024-06-12 21:41:32 +0700

    init LimitTypeModel

---
+++ src/databases/prisma/models/LimitType.ts
@@ -0,0 +1,54 @@
+import { createModel } from 'schemix';
+
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/LimitType';
+import { createdTime, deleted, updatedTime } from '../mixins';
+
+export default createModel(MODEL_NAME.LIMIT_TYPE, (LimitTypeModel) => {
+  const initCreatedTime = createdTime({
+    attribute: ATTRIBUTE.createdTime,
+    column: COLUMN.createdTime,
+  });
+  const initUpdatedTime = updatedTime({
+    attribute: ATTRIBUTE.updatedTime,
+    column: COLUMN.updatedTime,
+  });
+  const initDeleted = deleted(
+    {
+      attribute: ATTRIBUTE.deletedTime,
+      column: COLUMN.deletedTime,
+    },
+    {
+      attribute: ATTRIBUTE.isDeleted,
+      column: COLUMN.isDeleted,
+    },
+  );
+
+  // defined Model
+  process.nextTick(() => {
+    LimitTypeModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .string(ATTRIBUTE.name, {
+        map: COLUMN.name,
+        raw: RAW_STRING.LENGTH_45,
+      })
+      .string(ATTRIBUTE.name, {
+        map: COLUMN.name,
+        raw: RAW_STRING.LENGTH_200,
+        optional: true,
+      })
+
+      // dateTime marks
+      .mixin(initCreatedTime)
+      .mixin(initUpdatedTime)
+      .mixin(initDeleted)
+
+      // table name
+      .map(TABLE_NAME.LIMIT_TYPE);
+  });
+});
