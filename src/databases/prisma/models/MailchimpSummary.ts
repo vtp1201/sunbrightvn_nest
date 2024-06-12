9c186530579dc6a85210a534ac66d3c28442d90f
thang.pham <vtp1201@gmail.com>
2024-06-12 23:27:33 +0700

    init MailchimpSummaryModel

---
+++ src/databases/prisma/models/MailchimpSummary.ts
@@ -0,0 +1,71 @@
+import { createModel } from 'schemix';
+
+import { MODEL_NAME, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/MailchimpSummary';
+import { createdTime } from '../mixins';
+
+export default createModel(
+  MODEL_NAME.MAILCHIMP_SUMMARY,
+  (MailchimpSummaryModel) => {
+    const initCreatedTime = createdTime({
+      attribute: ATTRIBUTE.createdTime,
+      column: COLUMN.createdTime,
+    });
+
+    // defined Model
+    process.nextTick(() => {
+      MailchimpSummaryModel.int(ATTRIBUTE.id, {
+        id: true,
+        map: COLUMN.id,
+        default: {
+          autoincrement: true,
+        },
+      })
+        .int(ATTRIBUTE.open, {
+          map: COLUMN.open,
+          optional: true,
+        })
+        .int(ATTRIBUTE.uniqueOpen, {
+          map: COLUMN.uniqueOpen,
+          optional: true,
+        })
+        .int(ATTRIBUTE.click, {
+          map: COLUMN.click,
+          optional: true,
+        })
+        .int(ATTRIBUTE.subscriberClick, {
+          map: COLUMN.subscriberClick,
+          optional: true,
+        })
+        .int(ATTRIBUTE.emailSent, {
+          map: COLUMN.emailSent,
+          optional: true,
+        })
+        .int(ATTRIBUTE.unsubscribed, {
+          map: COLUMN.unsubscribed,
+          optional: true,
+        })
+        .int(ATTRIBUTE.bounced, {
+          map: COLUMN.bounced,
+          optional: true,
+        })
+        .int(ATTRIBUTE.abusedReport, {
+          map: COLUMN.abusedReport,
+          optional: true,
+        })
+        .int(ATTRIBUTE.mailchimpCampaignId, {
+          map: COLUMN.mailchimpCampaignId,
+          optional: true,
+        })
+
+        // dateTime marks
+        .mixin(initCreatedTime)
+
+        // indexes
+        .raw(INDEX.mailchimpCampaignId)
+
+        // table name
+        .map(TABLE_NAME.MAILCHIMP_SUMMARY);
+    });
+  },
+);
---
+++ src/databases/prisma/utils/enums/MailchimpSummary.ts
@@ -0,0 +1,42 @@
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
+
+export enum ATTRIBUTE {
+  id = 'id',
+  open = 'open',
+  uniqueOpen = 'uniqueOpen',
+  click = 'click',
+  subscriberClick = 'subscriberClick',
+  emailSent = 'emailSent',
+  unsubscribed = 'unsubscribed',
+  bounced = 'bounced',
+  abusedReport = 'abusedReport',
+  mailchimpCampaignId = 'mailchimpCampaignId',
+  createdTime = ATTRIBUTE_DEFAULT.createdTime,
+}
+
+export enum COLUMN {
+  id = 'id',
+  open = 'open',
+  uniqueOpen = 'unique_open',
+  click = 'click',
+  subscriberClick = 'subscriber_click',
+  emailSent = 'email_sent',
+  unsubscribed = 'unsubscribed',
+  bounced = 'bounced',
+  abusedReport = 'abused_report',
+  mailchimpCampaignId = 'mailchimp_campaign_id',
+  createdTime = COLUMN_DEFAULT.createdTime,
+}
+
+export enum RELATION {
+  mailchimpCampaign = RELATION_DEFAULT.mailchimpCampaign,
+}
+
+export enum INDEX {
+  mailchimpCampaignId = `@@index([${ATTRIBUTE.mailchimpCampaignId}], map: "fk_mai_mailchimp_campaign_id_mai_id_kf6o")`,
+}
+
+export default {
+  ATTRIBUTE,
+  RELATION,
+};
