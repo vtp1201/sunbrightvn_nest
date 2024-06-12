195255edbb1f5efb11b27a3f30963da25eeea13a
thang.pham <vtp1201@gmail.com>
2024-06-12 21:50:28 +0700

    init LimitValueModel

---
+++ src/databases/prisma/models/LimitValue.ts
@@ -0,0 +1,59 @@
+import { createModel } from 'schemix';
+
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/LimitValue';
+import { createdTime, deleted, updatedTime } from '../mixins';
+
+export default createModel(MODEL_NAME.LIMIT_VALUE, (LimitValueModel) => {
+  const initCreatedTime = createdTime({
+    attribute: ATTRIBUTE.createdTime,
+    column: COLUMN.createdTime,
+  });
+  const initUpdatedTime = updatedTime({
+    attribute: ATTRIBUTE.updatedTime,
+    column: COLUMN.updatedTime,
+  });
+  const initDeleted = deleted(
+    {
+      attribute: ATTRIBUTE.deletedTime,
+      column: COLUMN.deletedTime,
+    },
+    {
+      attribute: ATTRIBUTE.isDeleted,
+      column: COLUMN.isDeleted,
+    },
+  );
+
+  // defined Model
+  process.nextTick(() => {
+    LimitValueModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .int(ATTRIBUTE.limitId, {
+        map: COLUMN.limitId,
+      })
+      .string(ATTRIBUTE.name, {
+        map: COLUMN.name,
+        raw: RAW_STRING.LENGTH_45,
+      })
+      .string(ATTRIBUTE.description, {
+        map: COLUMN.description,
+        raw: RAW_STRING.LENGTH_200,
+        optional: true,
+      })
+      // dateTime marks
+      .mixin(initCreatedTime)
+      .mixin(initUpdatedTime)
+      .mixin(initDeleted)
+
+      // indexes
+      .raw(INDEX.limitId)
+
+      // table name
+      .map(TABLE_NAME.LIMIT_VALUE);
+  });
+});
---
+++ src/databases/prisma/utils/enums/LimitValue.ts
@@ -0,0 +1,36 @@
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
+
+export enum ATTRIBUTE {
+  id = 'id',
+  limitId = 'limitId',
+  name = 'name',
+  description = 'description',
+  createdTime = ATTRIBUTE_DEFAULT.createdTime,
+  updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
+  deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
+  isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
+}
+
+export enum COLUMN {
+  id = 'id',
+  limitId = 'limit_id',
+  name = 'name',
+  description = 'description',
+  createdTime = COLUMN_DEFAULT.createdTime,
+  updatedTime = COLUMN_DEFAULT.updatedTime,
+  deletedTime = COLUMN_DEFAULT.deletedTime,
+  isDeleted = COLUMN_DEFAULT.isDeleted,
+}
+
+export enum RELATION {
+  limit = RELATION_DEFAULT.limit,
+}
+
+export enum INDEX {
+  limitId = `@@index([${ATTRIBUTE.limitId}], map: "fk_limit_id_idx")`,
+}
+
+export default {
+  ATTRIBUTE,
+  RELATION,
+};
