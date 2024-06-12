5194087db5fdf5fb1d500b690c6dd5e45aec311c
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-12 12:48:28 +0700

    init GrantTypeAttributeModel

--- src/databases/prisma/models/GrantTypeAttribute.ts
+++ src/databases/prisma/models/GrantTypeAttribute.ts
@@ -1,7 +1,7 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
-import { ATTRIBUTE, COLUMN } from '../utils/enums/GrantTypeAttribute';
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/GrantTypeAttribute';
 import { createdTime, deleted, updatedTime } from '../mixins';
 
 export default createModel(
@@ -35,15 +35,53 @@ export default createModel(
           autoincrement: true,
         },
       })
+        .int(ATTRIBUTE.grantTypeId, {
+          map: COLUMN.grantTypeId,
+        })
+        .string(ATTRIBUTE.name, {
+          map: COLUMN.name,
+          raw: RAW_STRING.LENGTH_100,
+        })
+        .string(ATTRIBUTE.description, {
+          map: COLUMN.description,
+          raw: RAW_STRING.TEXT,
+          optional: true,
+        })
+        .int(ATTRIBUTE.parentId, {
+          map: COLUMN.parentId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.countryId, {
+          map: COLUMN.countryId,
+          optional: true,
+        })
+        .string(ATTRIBUTE.grantTypeAttributeCol, {
+          map: COLUMN.grantTypeAttributeCol,
+          raw: RAW_STRING.LENGTH_45,
+          optional: true,
+        })
+        .boolean(ATTRIBUTE.isDisabled, {
+          map: COLUMN.isDisabled,
+          optional: true,
+          default: false,
+        })
+        .int(ATTRIBUTE.order, {
+          map: COLUMN.order,
+          optional: true,
+        })
 
         // dateTime marks
         .mixin(initCreatedTime)
         .mixin(initUpdatedTime)
         .mixin(initDeleted)
 
+        // indexes
+        .raw(INDEX.countryId)
+        .raw(INDEX.grantTypeId)
+        .raw(INDEX.parentId)
+
         // table name
         .map(TABLE_NAME.GRANT_TYPE_ATTRIBUTE);
     });
   },
 );
-
--- src/databases/prisma/utils/enums/GrantTypeAttribute.ts
+++ src/databases/prisma/utils/enums/GrantTypeAttribute.ts
@@ -1,7 +1,15 @@
-import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT } from './default';
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
 
 export enum ATTRIBUTE {
   id = 'id',
+  grantTypeId = 'grantTypeId',
+  name = 'name',
+  description = 'description',
+  parentId = 'parentId',
+  countryId = 'countryId',
+  grantTypeAttributeCol = 'grantTypeAttributeCol',
+  isDisabled = 'isDisabled',
+  order = 'order',
   createdTime = ATTRIBUTE_DEFAULT.createdTime,
   updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
   deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
@@ -10,6 +18,14 @@ export enum ATTRIBUTE {
 
 export enum COLUMN {
   id = 'id',
+  grantTypeId = 'grant_type_id',
+  name = 'name',
+  description = 'description',
+  parentId = 'parent_id',
+  countryId = 'country_id',
+  grantTypeAttributeCol = 'grant_type_attribute_col',
+  isDisabled = 'is_disabled',
+  order = 'order',
   createdTime = COLUMN_DEFAULT.createdTime,
   updatedTime = COLUMN_DEFAULT.updatedTime,
   deletedTime = COLUMN_DEFAULT.deletedTime,
@@ -17,11 +33,28 @@ export enum COLUMN {
 }
 
 export enum RELATION {
+  country = RELATION_DEFAULT.country,
+}
+// grantSchemeHasGrantTypeAttributeAgency = RELATION_DEFAULT.grantSchemeHasGrantTypeAttributeAgency,
+// grantSchemeHasGrantTypeAttributeBusinessSize = RELATION_DEFAULT.grantSchemeHasGrantTypeAttributeBusinessSize,
+// grantSchemeHasGrantTypeAttributeBusinessStage = RELATION_DEFAULT.grantSchemeHasGrantTypeAttributeBusinessStage,
+// grantSchemeHasGrantTypeAttributeIndustry = RELATION_DEFAULT.grantSchemeHasGrantTypeAttributeIndustry,
+// grantSchemeHasGrantTypeAttributeLocalShareholding = RELATION_DEFAULT.grantSchemeHasGrantTypeAttributeLocalShareholding,
+// grantSchemeHasGrantTypeAttributeNeedsBased = RELATION_DEFAULT.grantSchemeHasGrantTypeAttributeNeedsBased,
+// grantSchemeHasGrantTypeAttributeNumberEmployees = RELATION_DEFAULT.grantSchemeHasGrantTypeAttributeNumberEmployees,
+// grantSchemeHasGrantTypeAttributeType = RELATION_DEFAULT.grantSchemeHasGrantTypeAttributeType,
+// grantSchemeHasGrantTypeAttributeYearsOperation = RELATION_DEFAULT.grantSchemeHasGrantTypeAttributeYearsOperation,
+// grantTypeAttribute = RELATION_DEFAULT.grantTypeAttribute,
+// otherGrantTypeAttribute = RELATION_DEFAULT.otherGrantTypeAttribute,
+// grantType = RELATION_DEFAULT.grantType,
 
+export enum INDEX {
+  countryId = `@@index([${ATTRIBUTE.countryId}], map: "fk_grant_type_attribute_country_idx")`,
+  parentId = `@@index([${ATTRIBUTE.parentId}], map: "fk_grant_type_attribute_self_idx")`,
+  grantTypeId = `@@index([${ATTRIBUTE.grantTypeId}], map: "fk_grant_type_attribute_type_idx")`,
 }
 
 export default {
   ATTRIBUTE,
   RELATION,
 };
-
