8f4b243bf0e75580d2f26d1c817e22fcd9e1e492
thang.pham <vtp1201@gmail.com>
2024-06-13 00:20:02 +0700

    init NotificationTypeModel

---
+++ src/databases/prisma/utils/enums/NotificationType.ts
@@ -0,0 +1,29 @@
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
+
+export enum ATTRIBUTE {
+  id = 'id',
+  name = 'name',
+  createdTime = ATTRIBUTE_DEFAULT.createdTime,
+  updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
+  deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
+  isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
+}
+
+export enum COLUMN {
+  id = 'id',
+  name = 'name',
+  createdTime = COLUMN_DEFAULT.createdTime,
+  updatedTime = COLUMN_DEFAULT.updatedTime,
+  deletedTime = COLUMN_DEFAULT.deletedTime,
+  isDeleted = COLUMN_DEFAULT.isDeleted,
+}
+
+export enum RELATION {
+  notifications = RELATION_DEFAULT.notifications,
+  notificationTemplates = RELATION_DEFAULT.notificationTemplates,
+}
+
+export default {
+  ATTRIBUTE,
+  RELATION,
+};
