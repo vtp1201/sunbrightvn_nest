b090ca98cfd871b483d03b80af3283e635486523
thang.pham <vtp1201@gmail.com>
2024-06-13 00:10:57 +0700

    init NotificationStatusModel

---
+++ src/databases/prisma/models/NotificationStatus.ts
@@ -0,0 +1,52 @@
+import { createModel } from 'schemix';
+
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/NotificationStatus';
+import { createdTime, deleted, updatedTime } from '../mixins';
+
+export default createModel(
+  MODEL_NAME.NOTIFICATION_STATUS,
+  (NotificationStatusModel) => {
+    const initCreatedTime = createdTime({
+      attribute: ATTRIBUTE.createdTime,
+      column: COLUMN.createdTime,
+    });
+    const initUpdatedTime = updatedTime({
+      attribute: ATTRIBUTE.updatedTime,
+      column: COLUMN.updatedTime,
+    });
+    const initDeleted = deleted(
+      {
+        attribute: ATTRIBUTE.deletedTime,
+        column: COLUMN.deletedTime,
+      },
+      {
+        attribute: ATTRIBUTE.isDeleted,
+        column: COLUMN.isDeleted,
+      },
+    );
+
+    // defined Model
+    process.nextTick(() => {
+      NotificationStatusModel.int(ATTRIBUTE.id, {
+        id: true,
+        map: COLUMN.id,
+        default: {
+          autoincrement: true,
+        },
+      })
+        .string(ATTRIBUTE.name, {
+          map: COLUMN.name,
+          raw: RAW_STRING.LENGTH_45,
+        })
+
+        // dateTime marks
+        .mixin(initCreatedTime)
+        .mixin(initUpdatedTime)
+        .mixin(initDeleted)
+
+        // table name
+        .map(TABLE_NAME.NOTIFICATION_STATUS);
+    });
+  },
+);
