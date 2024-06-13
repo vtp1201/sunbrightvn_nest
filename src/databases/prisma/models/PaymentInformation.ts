188406b24e17d7c4f88d253e2b88f248936059f2
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-13 16:05:27 +0700

    init PaymentInformationModel

--- src/databases/prisma/models/PaymentInformation.ts
+++ src/databases/prisma/models/PaymentInformation.ts
@@ -1,6 +1,6 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
+import { MODEL_NAME, RAW_DATE_TIME, RAW_STRING, TABLE_NAME } from '../utils';
 import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/PaymentInformation';
 import { createdTime, deleted, updatedTime } from '../mixins';
 
@@ -35,6 +35,50 @@ export default createModel(
           autoincrement: true,
         },
       })
+        .int(ATTRIBUTE.companyMemberId, {
+          map: COLUMN.companyMemberId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.orderId, {
+          map: COLUMN.orderId,
+          optional: true,
+        })
+        .int(ATTRIBUTE.taskId, {
+          map: COLUMN.taskId,
+          optional: true,
+        })
+        .dateTime(ATTRIBUTE.dateOfPayment, {
+          map: COLUMN.dateOfPayment,
+          optional: true,
+        })
+        .int(ATTRIBUTE.paymentAmount, {
+          map: COLUMN.paymentAmount,
+          optional: true,
+        })
+        .string(ATTRIBUTE.nameOnCard, {
+          map: COLUMN.nameOnCard,
+          raw: RAW_STRING.LENGTH_255,
+          optional: true,
+        })
+        .int(ATTRIBUTE.lastFourDigitsOnCard, {
+          map: COLUMN.lastFourDigitsOnCard,
+          optional: true,
+        })
+        .dateTime(ATTRIBUTE.expiredDateOfCard, {
+          map: COLUMN.expiredDateOfCard,
+          raw: RAW_DATE_TIME.DATE_ONLY,
+          optional: true,
+        })
+        .string(ATTRIBUTE.issuingBank, {
+          map: COLUMN.issuingBank,
+          raw: RAW_STRING.LENGTH_255,
+          optional: true,
+        })
+        .string(ATTRIBUTE.emailAddressOfCardholder, {
+          map: COLUMN.emailAddressOfCardholder,
+          raw: RAW_STRING.LENGTH_255,
+          optional: true,
+        })
 
         // dateTime marks
         .mixin(initCreatedTime)
@@ -42,7 +86,9 @@ export default createModel(
         .mixin(initDeleted)
 
         // indexes
-        .raw()
+        .raw(INDEX.companyMemberId)
+        .raw(INDEX.orderId)
+        .raw(INDEX.taskId)
 
         // table name
         .map(TABLE_NAME.PAYMENT_INFORMATION);
--- src/databases/prisma/utils/enums/PaymentInformation.ts
+++ src/databases/prisma/utils/enums/PaymentInformation.ts
@@ -1,7 +1,17 @@
-import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT } from './default';
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
 
 export enum ATTRIBUTE {
   id = 'id',
+  companyMemberId = 'companyMemberId',
+  orderId = 'orderId',
+  taskId = 'taskId',
+  dateOfPayment = 'dateOfPayment',
+  paymentAmount = 'paymentAmount',
+  nameOnCard = 'nameOnCard',
+  lastFourDigitsOnCard = 'lastFourDigitsOnCard',
+  expiredDateOfCard = 'expiredDateOfCard',
+  issuingBank = 'issuingBank',
+  emailAddressOfCardholder = 'emailAddressOfCardholder',
   createdTime = ATTRIBUTE_DEFAULT.createdTime,
   updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
   deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
@@ -10,6 +20,16 @@ export enum ATTRIBUTE {
 
 export enum COLUMN {
   id = 'id',
+  companyMemberId = 'company_member_id',
+  orderId = 'order_id',
+  taskId = 'task_id',
+  dateOfPayment = 'date_of_payment',
+  paymentAmount = 'payment_amount',
+  nameOnCard = 'name_on_card',
+  lastFourDigitsOnCard = 'last_four_digits_on_card',
+  expiredDateOfCard = 'expired_date_of_card',
+  issuingBank = 'issuing_bank',
+  emailAddressOfCardholder = 'email_address_of_cardholder',
   createdTime = COLUMN_DEFAULT.createdTime,
   updatedTime = COLUMN_DEFAULT.updatedTime,
   deletedTime = COLUMN_DEFAULT.deletedTime,
@@ -17,15 +37,18 @@ export enum COLUMN {
 }
 
 export enum RELATION {
-
+  companyMember = RELATION_DEFAULT.companyMember,
+  order = RELATION_DEFAULT.order,
+  task = RELATION_DEFAULT.task,
 }
 
 export enum INDEX {
-
+  companyMemberId = `@@index([${ATTRIBUTE.companyMemberId}], map: "fk_pay_company_member_id_com_id_1axx")`,
+  orderId = `@@index([${ATTRIBUTE.orderId}], map: "fk_pay_order_id_ord_id_atc9")`,
+  taskId = `@@index([${ATTRIBUTE.taskId}], map: "fk_pay_task_id_tas_id_3ro9")`,
 }
 
 export default {
   ATTRIBUTE,
   RELATION,
 };
-
