1ae22f1721c925fd7c0afb8935d371e34b0752fa
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-13 12:44:55 +0700

    init OrderItemModel

--- src/databases/prisma/models/OrderItem.ts
+++ src/databases/prisma/models/OrderItem.ts
@@ -1,52 +1,93 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
+import { MODEL_NAME, RAW_NUMBER, RAW_STRING, TABLE_NAME } from '../utils';
 import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/OrderItem';
-import { createdTime, deleted, updatedTime } from '../mixins';
+import { createdTime, updatedTime } from '../mixins';
 
-export default createModel(
-  MODEL_NAME.ORDER_ITEM,
-  (OrderItemModel) => {
-    const initCreatedTime = createdTime({
-      attribute: ATTRIBUTE.createdTime,
-      column: COLUMN.createdTime,
-    });
-    const initUpdatedTime = updatedTime({
-      attribute: ATTRIBUTE.updatedTime,
-      column: COLUMN.updatedTime,
-    });
-    const initDeleted = deleted(
-      {
-        attribute: ATTRIBUTE.deletedTime,
-        column: COLUMN.deletedTime,
-      },
-      {
-        attribute: ATTRIBUTE.isDeleted,
-        column: COLUMN.isDeleted,
-      },
-    );
+export default createModel(MODEL_NAME.ORDER_ITEM, (OrderItemModel) => {
+  const initCreatedTime = createdTime({
+    attribute: ATTRIBUTE.createdTime,
+    column: COLUMN.createdTime,
+  });
+  const initUpdatedTime = updatedTime({
+    attribute: ATTRIBUTE.updatedTime,
+    column: COLUMN.updatedTime,
+  });
 
-    // defined Model
-    process.nextTick(() => {
-      OrderItemModel.int(ATTRIBUTE.id, {
-        id: true,
-        map: COLUMN.id,
-        default: {
-          autoincrement: true,
-        },
+  // defined Model
+  process.nextTick(() => {
+    OrderItemModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .int(ATTRIBUTE.orderId, {
+        map: COLUMN.orderId,
+      })
+      .int(ATTRIBUTE.serviceId, {
+        map: COLUMN.serviceId,
+      })
+      .int(ATTRIBUTE.packageId, {
+        map: COLUMN.packageId,
+        optional: true,
+      })
+      .float(ATTRIBUTE.unitPrice, {
+        map: COLUMN.unitPrice,
+        raw: RAW_NUMBER.FLOAT,
+        optional: true,
+      })
+      .int(ATTRIBUTE.quantity, {
+        map: COLUMN.quantity,
+        optional: true,
+      })
+      .float(ATTRIBUTE.amount, {
+        map: COLUMN.amount,
+        raw: RAW_NUMBER.FLOAT,
+        optional: true,
+      })
+      .string(ATTRIBUTE.description, {
+        map: COLUMN.description,
+        raw: RAW_STRING.TEXT,
+        optional: true,
+      })
+      .int(ATTRIBUTE.voucherId, {
+        map: COLUMN.voucherId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.promotionId, {
+        map: COLUMN.promotionId,
+        optional: true,
+      })
+      .boolean(ATTRIBUTE.isRefund, {
+        map: COLUMN.isRefund,
+        default: false,
+      })
+      .int(ATTRIBUTE.discountOrderItemId, {
+        map: COLUMN.discountOrderItemId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.orderItemStatusId, {
+        map: COLUMN.orderItemStatusId,
+        default: 0,
+      })
+      .dateTime(ATTRIBUTE.finishedTime, {
+        raw: COLUMN.finishedTime,
+        optional: true,
       })
 
-        // dateTime marks
-        .mixin(initCreatedTime)
-        .mixin(initUpdatedTime)
-        .mixin(initDeleted)
+      // dateTime marks
+      .mixin(initCreatedTime)
+      .mixin(initUpdatedTime)
 
-        // indexes
-        .raw()
+      // indexes
+      .raw(INDEX.orderId)
+      .raw(INDEX.orderItemStatusId)
+      .raw(INDEX.packageId)
+      .raw(INDEX.serviceId)
 
-        // table name
-        .map(TABLE_NAME.ORDER_ITEM);
-    });
-  },
-);
-
+      // table name
+      .map(TABLE_NAME.ORDER_ITEM);
+  });
+});
--- src/databases/prisma/utils/enums/OrderItem.ts
+++ src/databases/prisma/utils/enums/OrderItem.ts
@@ -1,31 +1,58 @@
-import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT } from './default';
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
 
 export enum ATTRIBUTE {
   id = 'id',
+  orderId = 'orderId',
+  serviceId = 'serviceId',
+  packageId = 'packageId',
+  unitPrice = 'unitPrice',
+  quantity = 'quantity',
+  amount = 'amount',
+  description = 'description',
+  voucherId = 'voucherId',
+  promotionId = 'promotionId',
+  isRefund = 'isRefund',
+  discountOrderItemId = 'discountOrderItemId',
+  orderItemStatusId = 'orderItemStatusId',
+  finishedTime = ATTRIBUTE_DEFAULT.finishedTime,
   createdTime = ATTRIBUTE_DEFAULT.createdTime,
   updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
-  deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
-  isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
 }
 
 export enum COLUMN {
   id = 'id',
+  orderId = 'order_id',
+  serviceId = 'service_id',
+  packageId = 'package_id',
+  unitPrice = 'unit_price',
+  quantity = 'quantity',
+  amount = 'amount',
+  description = 'description',
+  voucherId = 'voucher_id',
+  promotionId = 'promotion_id',
+  isRefund = 'is_refund',
+  discountOrderItemId = 'discount_order_item_id',
+  orderItemStatusId = 'order_item_status_id',
+  finishedTime = COLUMN_DEFAULT.finishedTime,
   createdTime = COLUMN_DEFAULT.createdTime,
   updatedTime = COLUMN_DEFAULT.updatedTime,
-  deletedTime = COLUMN_DEFAULT.deletedTime,
-  isDeleted = COLUMN_DEFAULT.isDeleted,
 }
 
 export enum RELATION {
-
+  order = RELATION_DEFAULT.order,
+  orderItemStatus = RELATION_DEFAULT.orderItemStatus,
+  renamedPackage = RELATION_DEFAULT.renamedPackage,
+  service = RELATION_DEFAULT.service,
 }
 
 export enum INDEX {
-
+  orderId = `@@index([${ATTRIBUTE.orderId}], map: "fk_ord_order_id_ord_id_fvmb")`,
+  orderItemStatusId = `@@index([${ATTRIBUTE.orderItemStatusId}], map: "fk_ord_order_item_status_id_ord_id_8qd8")`,
+  packageId = `@@index([${ATTRIBUTE.packageId}], map: "fk_ord_package_id_pac_id_3934")`,
+  serviceId = `@@index([${ATTRIBUTE.serviceId}], map: "fk_ord_service_id_ser_id_hwno")`,
 }
 
 export default {
   ATTRIBUTE,
   RELATION,
 };
-
