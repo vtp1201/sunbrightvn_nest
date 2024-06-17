2ba118a140876007a64b43089054f3f2d918d541
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-17 11:51:39 +0700

    init ProcessLogModel

--- src/databases/prisma/models/ProcessLog.ts
+++ src/databases/prisma/models/ProcessLog.ts
@@ -1,17 +1,13 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
 import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/ProcessLog';
-import { createdTime, deleted, updatedTime } from '../mixins';
+import { createdTime, deleted } from '../mixins';
 
 export default createModel(MODEL_NAME.PROCESS_LOG, (ProcessLogModel) => {
   const initCreatedTime = createdTime({
     attribute: ATTRIBUTE.createdTime,
     column: COLUMN.createdTime,
-  });
-  const initUpdatedTime = updatedTime({
-    attribute: ATTRIBUTE.updatedTime,
-    column: COLUMN.updatedTime,
   });
   const initDeleted = deleted(
     {
@@ -33,14 +29,61 @@ export default createModel(MODEL_NAME.PROCESS_LOG, (ProcessLogModel) => {
         autoincrement: true,
       },
     })
+      .int(ATTRIBUTE.processId, {
+        map: COLUMN.processId,
+      })
+      .boolean(ATTRIBUTE.isFinished, {
+        map: COLUMN.isFinished,
+        default: false,
+      })
+      .dateTime(ATTRIBUTE.finishedTime, {
+        map: COLUMN.finishedTime,
+        optional: true,
+      })
+      .boolean(ATTRIBUTE.isLogMail, {
+        map: COLUMN.isLogMail,
+        default: false,
+      })
+      .boolean(ATTRIBUTE.isRemind, {
+        map: COLUMN.isRemind,
+        default: false,
+      })
+      .string(ATTRIBUTE.description, {
+        map: COLUMN.description,
+        optional: true,
+        raw: RAW_STRING.LENGTH_255,
+      })
+      .int(ATTRIBUTE.agentId, {
+        map: COLUMN.agentId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.fileTemplateId, {
+        map: COLUMN.fileTemplateId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.companyMemberId, {
+        map: COLUMN.companyMemberId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.belongToCompanyMemberId, {
+        map: COLUMN.belongToCompanyMemberId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.emailTemplateId, {
+        map: COLUMN.emailTemplateId,
+        optional: true,
+      })
 
       // dateTime marks
       .mixin(initCreatedTime)
-      .mixin(initUpdatedTime)
       .mixin(initDeleted)
 
       // indexes
-      .raw()
+      .raw(INDEX.agentId)
+      .raw(INDEX.belongToCompanyMemberId)
+      .raw(INDEX.companyMemberId)
+      .raw(INDEX.fileTemplateId)
+      .raw(INDEX.processId)
 
       // table name
       .map(TABLE_NAME.PROCESS_LOG);
--- src/databases/prisma/utils/enums/ProcessLog.ts
+++ src/databases/prisma/utils/enums/ProcessLog.ts
@@ -1,31 +1,58 @@
-import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT } from './default';
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
 
 export enum ATTRIBUTE {
   id = 'id',
+  processId = 'processId',
+  isFinished = 'isFinished',
+  finishedTime = ATTRIBUTE_DEFAULT.finishedTime,
+  isLogMail = 'isLogMail',
+  isRemind = 'isRemind',
+  description = 'description',
+  agentId = 'agentId',
+  fileTemplateId = 'fileTemplateId',
+  companyMemberId = 'companyMemberId',
+  belongToCompanyMemberId = 'belongToCompanyMemberId',
+  emailTemplateId = 'emailTemplateId',
   createdTime = ATTRIBUTE_DEFAULT.createdTime,
-  updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
   deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
   isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
 }
 
 export enum COLUMN {
   id = 'id',
+  processId = 'process_id',
+  isFinished = 'is_finished',
+  finishedTime = COLUMN_DEFAULT.finishedTime,
+  isLogMail = 'is_log_mail',
+  isRemind = 'is_remind',
+  description = 'description',
+  agentId = 'agent_id',
+  fileTemplateId = 'file_template_id',
+  companyMemberId = 'company_member_id',
+  belongToCompanyMemberId = 'belong_to_company_member_id',
+  emailTemplateId = 'email_template_id',
   createdTime = COLUMN_DEFAULT.createdTime,
-  updatedTime = COLUMN_DEFAULT.updatedTime,
   deletedTime = COLUMN_DEFAULT.deletedTime,
   isDeleted = COLUMN_DEFAULT.isDeleted,
 }
 
 export enum RELATION {
-
+  agent = 'agent',
+  companyMemberBelongTo = 'company_member_process_log_belong_to_company_member_idTocompany_member',
+  companyMember = 'company_member_process_log_company_member_idTocompany_member',
+  fileTemplate = 'file_template',
+  process = 'process',
 }
 
 export enum INDEX {
-
+  agentId = `@@index([${ATTRIBUTE.agentId}], map: "fk_pro_agent_id_age_id_6pr9")`,
+  belongToCompanyMemberId = `@@index([${ATTRIBUTE.belongToCompanyMemberId}], map: "fk_pro_belong_to_company_member_id_com_id_f9aw")`,
+  companyMemberId = `@@index([${ATTRIBUTE.companyMemberId}], map: "fk_pro_company_member_id_com_id_e3mj")`,
+  fileTemplateId = `@@index([${ATTRIBUTE.fileTemplateId}], map: "fk_pro_file_template_id_fil_id_c3wc")`,
+  processId = `@@index([${ATTRIBUTE.processId}], map: "fk_pro_process_id_pro_id_1wjq")`,
 }
 
 export default {
   ATTRIBUTE,
   RELATION,
 };
-
