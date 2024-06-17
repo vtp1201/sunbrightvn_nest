469e7471ece73c6dd29d00cb87baad221451837a
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-17 12:46:19 +0700

    init PromotionModel

--- src/databases/prisma/models/Promotion.ts
+++ src/databases/prisma/models/Promotion.ts
@@ -1,17 +1,13 @@
 import { createModel } from 'schemix';
 
 import { MODEL_NAME, TABLE_NAME } from '../utils';
-import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/Promotion';
-import { createdTime, deleted, updatedTime } from '../mixins';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/Promotion';
+import { createdTime, deleted } from '../mixins';
 
 export default createModel(MODEL_NAME.PROMOTION, (PromotionModel) => {
   const initCreatedTime = createdTime({
     attribute: ATTRIBUTE.createdTime,
     column: COLUMN.createdTime,
-  });
-  const initUpdatedTime = updatedTime({
-    attribute: ATTRIBUTE.updatedTime,
-    column: COLUMN.updatedTime,
   });
   const initDeleted = deleted(
     {
@@ -33,14 +29,25 @@ export default createModel(MODEL_NAME.PROMOTION, (PromotionModel) => {
         autoincrement: true,
       },
     })
+      .string(ATTRIBUTE.promotionCode, {
+        map: COLUMN.promotionCode,
+      })
+      .int(ATTRIBUTE.percent, {
+        map: COLUMN.percent,
+        default: 0,
+      })
+      .int(ATTRIBUTE.amount, {
+        map: COLUMN.amount,
+        default: 0,
+      })
+      .boolean(ATTRIBUTE.isUsed, {
+        map: COLUMN.isUsed,
+        default: false,
+      })
 
       // dateTime marks
       .mixin(initCreatedTime)
-      .mixin(initUpdatedTime)
       .mixin(initDeleted)
-
-      // indexes
-      .raw()
 
       // table name
       .map(TABLE_NAME.PROMOTION);
--- src/databases/prisma/utils/enums/Promotion.ts
+++ src/databases/prisma/utils/enums/Promotion.ts
@@ -2,30 +2,29 @@ import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT } from './default';
 
 export enum ATTRIBUTE {
   id = 'id',
+  promotionCode = 'promotionCode',
+  percent = 'percent',
+  amount = 'amount',
+  isUsed = 'isUsed',
   createdTime = ATTRIBUTE_DEFAULT.createdTime,
-  updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
   deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
   isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
 }
 
 export enum COLUMN {
   id = 'id',
+  promotionCode = 'promotion_code',
+  percent = 'percent',
+  amount = 'amount',
+  isUsed = 'is_used',
   createdTime = COLUMN_DEFAULT.createdTime,
-  updatedTime = COLUMN_DEFAULT.updatedTime,
   deletedTime = COLUMN_DEFAULT.deletedTime,
   isDeleted = COLUMN_DEFAULT.isDeleted,
 }
 
-export enum RELATION {
-
-}
-
-export enum INDEX {
-
-}
+export enum RELATION {}
 
 export default {
   ATTRIBUTE,
   RELATION,
 };
-
