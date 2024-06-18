3401b39523463db66909b03e82b5ea0007210f7a
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-18 17:39:37 +0700

    init SurveyModel

--- src/databases/prisma/utils/enums/default.ts
+++ src/databases/prisma/utils/enums/default.ts
@@ -17,6 +17,7 @@ export enum COLUMN_DEFAULT {
 }
 
 export enum RELATION_DEFAULT {
+  surveyChoices = 'surveyChoices',
   supportCases = 'supportCases',
   messages = 'messages',
   supportCaseStatus = 'supportCaseStatus',
