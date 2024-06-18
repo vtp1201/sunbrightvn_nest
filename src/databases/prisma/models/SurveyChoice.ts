422867eeaf8203f4d1e79a400453ebb144f5cf97
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-18 17:44:21 +0700

    init SurveyChoiceModel

--- src/databases/prisma/models/SurveyChoice.ts
+++ src/databases/prisma/models/SurveyChoice.ts
@@ -1,52 +1,49 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
 import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/SurveyChoice';
-import { createdTime, deleted, updatedTime } from '../mixins';
+import { deleted } from '../mixins';
 
-export default createModel(
-  MODEL_NAME.SURVEY_CHOICE,
-  (SurveyChoiceModel) => {
-    const initCreatedTime = createdTime({
-      attribute: ATTRIBUTE.createdTime,
-      column: COLUMN.createdTime,
-    });
-    const initUpdatedTime = updatedTime({
-      attribute: ATTRIBUTE.updatedTime,
-      column: COLUMN.updatedTime,
-    });
-    const initDeleted = deleted(
-      {
-        attribute: ATTRIBUTE.deletedTime,
-        column: COLUMN.deletedTime,
-      },
-      {
-        attribute: ATTRIBUTE.isDeleted,
-        column: COLUMN.isDeleted,
-      },
-    );
+export default createModel(MODEL_NAME.SURVEY_CHOICE, (SurveyChoiceModel) => {
+  const initDeleted = deleted(
+    {
+      attribute: ATTRIBUTE.deletedTime,
+      column: COLUMN.deletedTime,
+    },
+    {
+      attribute: ATTRIBUTE.isDeleted,
+      column: COLUMN.isDeleted,
+    },
+  );
 
-    // defined Model
-    process.nextTick(() => {
-      SurveyChoiceModel.int(ATTRIBUTE.id, {
-        id: true,
-        map: COLUMN.id,
-        default: {
-          autoincrement: true,
-        },
+  // defined Model
+  process.nextTick(() => {
+    SurveyChoiceModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .int(ATTRIBUTE.surveyId, {
+        map: COLUMN.surveyId,
+      })
+      .string(ATTRIBUTE.name, {
+        map: COLUMN.name,
+        raw: RAW_STRING.TEXT,
+      })
+      .boolean(ATTRIBUTE.isContent, {
+        map: COLUMN.isContent,
+        default: false,
       })
 
-        // dateTime marks
-        .mixin(initCreatedTime)
-        .mixin(initUpdatedTime)
-        .mixin(initDeleted)
+      // dateTime marks
+      .mixin(initDeleted)
 
-        // indexes
-        .raw()
+      // indexes
+      .raw(INDEX.surveyId)
 
-        // table name
-        .map(TABLE_NAME.SURVEY_CHOICE);
-    });
-  },
-);
-
+      // table name
+      .map(TABLE_NAME.SURVEY_CHOICE);
+  });
+});
--- src/databases/prisma/utils/enums/SurveyChoice.ts
+++ src/databases/prisma/utils/enums/SurveyChoice.ts
@@ -2,30 +2,29 @@ import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT } from './default';
 
 export enum ATTRIBUTE {
   id = 'id',
-  createdTime = ATTRIBUTE_DEFAULT.createdTime,
-  updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
+  surveyId = 'surveyId',
+  name = 'name',
+  isContent = 'isContent',
   deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
   isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
 }
 
 export enum COLUMN {
   id = 'id',
-  createdTime = COLUMN_DEFAULT.createdTime,
-  updatedTime = COLUMN_DEFAULT.updatedTime,
+  surveyId = 'survey_id',
+  name = 'name',
+  isContent = 'is_content',
   deletedTime = COLUMN_DEFAULT.deletedTime,
   isDeleted = COLUMN_DEFAULT.isDeleted,
 }
 
-export enum RELATION {
-
-}
+export enum RELATION {}
 
 export enum INDEX {
-
+  surveyId = `@@index([${ATTRIBUTE.surveyId}], map: "fk_survey_choice_survey_idx")`,
 }
 
 export default {
   ATTRIBUTE,
   RELATION,
 };
-
