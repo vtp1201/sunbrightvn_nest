409db6e7015b6fd66572220215f0598969071ecc
thang.pham <vtp1201@gmail.com>
2024-06-19 00:19:26 +0700

    init TokenModel

--- src/databases/prisma/models/Token.ts
+++ src/databases/prisma/models/Token.ts
@@ -1,52 +1,123 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
 import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/Token';
-import { createdTime, deleted, updatedTime } from '../mixins';
+import { createdTime, updatedTime } from '../mixins';
 
-export default createModel(
-  MODEL_NAME.TOKEN,
-  (TokenModel) => {
-    const initCreatedTime = createdTime({
-      attribute: ATTRIBUTE.createdTime,
-      column: COLUMN.createdTime,
-    });
-    const initUpdatedTime = updatedTime({
-      attribute: ATTRIBUTE.updatedTime,
-      column: COLUMN.updatedTime,
-    });
-    const initDeleted = deleted(
-      {
-        attribute: ATTRIBUTE.deletedTime,
-        column: COLUMN.deletedTime,
-      },
-      {
-        attribute: ATTRIBUTE.isDeleted,
-        column: COLUMN.isDeleted,
-      },
-    );
+export default createModel(MODEL_NAME.TOKEN, (TokenModel) => {
+  const initCreatedTime = createdTime({
+    attribute: ATTRIBUTE.createdTime,
+    column: COLUMN.createdTime,
+  });
+  const initUpdatedTime = updatedTime({
+    attribute: ATTRIBUTE.updatedTime,
+    column: COLUMN.updatedTime,
+  });
 
-    // defined Model
-    process.nextTick(() => {
-      TokenModel.int(ATTRIBUTE.id, {
-        id: true,
-        map: COLUMN.id,
-        default: {
-          autoincrement: true,
-        },
+  // defined Model
+  process.nextTick(() => {
+    TokenModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .int(ATTRIBUTE.userId, {
+        map: COLUMN.userId,
+      })
+      .string(ATTRIBUTE.accessToken, {
+        map: COLUMN.accessToken,
+        raw: RAW_STRING.LENGTH_255,
+      })
+      .int(ATTRIBUTE.accessTokenExp, {
+        map: COLUMN.accessTokenExp,
+      })
+      .int(ATTRIBUTE.accessTokenIat, {
+        map: COLUMN.accessTokenIat,
+      })
+      .string(ATTRIBUTE.refreshToken, {
+        map: COLUMN.refreshToken,
+        raw: RAW_STRING.LENGTH_255,
+      })
+      .int(ATTRIBUTE.refreshTokenExp, {
+        map: COLUMN.refreshTokenExp,
+      })
+      .int(ATTRIBUTE.refreshTokenIat, {
+        map: COLUMN.refreshTokenIat,
+      })
+      .string(ATTRIBUTE.scope, {
+        map: COLUMN.scope,
+        raw: RAW_STRING.LENGTH_255,
+        optional: true,
+      })
+      .int(ATTRIBUTE.tokenType, {
+        map: COLUMN.tokenType,
+        optional: true,
+      })
+      .string(ATTRIBUTE.xeroTokenType, {
+        map: COLUMN.xeroTokenType,
+        raw: RAW_STRING.LENGTH_255,
+        optional: true,
+      })
+      .string(ATTRIBUTE.xeroIdToken, {
+        map: COLUMN.xeroIdToken,
+        raw: RAW_STRING.LENGTH_255,
+        optional: true,
+      })
+      .string(ATTRIBUTE.xeroSessionState, {
+        map: COLUMN.xeroSessionState,
+        raw: RAW_STRING.LENGTH_255,
+        optional: true,
+      })
+      .int(ATTRIBUTE.websiteId, {
+        map: COLUMN.websiteId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.taskId, {
+        map: COLUMN.taskId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.agentId, {
+        map: COLUMN.agentId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.companyMemberId, {
+        map: COLUMN.companyMemberId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.fileTemplateId, {
+        map: COLUMN.fileTemplateId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.belongToCompanyMemberId, {
+        map: COLUMN.belongToCompanyMemberId,
+        optional: true,
+      })
+      .string(ATTRIBUTE.ipAddress, {
+        map: COLUMN.ipAddress,
+        raw: RAW_STRING.LENGTH_255,
+        optional: true,
+      })
+      .int(ATTRIBUTE.countRequest, {
+        map: COLUMN.countRequest,
+        optional: true,
       })
 
-        // dateTime marks
-        .mixin(initCreatedTime)
-        .mixin(initUpdatedTime)
-        .mixin(initDeleted)
+      // dateTime marks
+      .mixin(initCreatedTime)
+      .mixin(initUpdatedTime)
 
-        // indexes
-        .raw()
+      // indexes
+      .raw(INDEX.agentId)
+      .raw(INDEX.belongToCompanyMemberId)
+      .raw(INDEX.companyMemberId)
+      .raw(INDEX.fileTemplateId)
+      .raw(INDEX.taskId)
+      .raw(INDEX.userId)
+      .raw(INDEX.websiteId)
 
-        // table name
-        .map(TABLE_NAME.TOKEN);
-    });
-  },
-);
-
+      // table name
+      .map(TABLE_NAME.TOKEN);
+  });
+});
--- src/databases/prisma/utils/enums/Token.ts
+++ src/databases/prisma/utils/enums/Token.ts
@@ -1,31 +1,78 @@
-import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT } from './default';
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
 
 export enum ATTRIBUTE {
   id = 'id',
+  userId = 'userId',
+  accessToken = 'accessToken',
+  accessTokenExp = 'accessTokenExp',
+  accessTokenIat = 'accessTokenIat',
+  refreshToken = 'refreshToken',
+  refreshTokenExp = 'refreshTokenExp',
+  refreshTokenIat = 'refreshTokenIat',
+  scope = 'scope',
+  tokenType = 'tokenType',
+  xeroTokenType = 'xeroTokenType',
+  xeroIdToken = 'xeroIdToken',
+  xeroSessionState = 'xeroSessionState',
+  websiteId = 'websiteId',
+  taskId = 'taskId',
+  agentId = 'agentId',
+  companyMemberId = 'companyMemberId',
+  fileTemplateId = 'fileTemplateId',
+  belongToCompanyMemberId = 'belongToCompanyMemberId',
+  ipAddress = 'ipAddress',
+  countRequest = 'countRequest',
   createdTime = ATTRIBUTE_DEFAULT.createdTime,
   updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
-  deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
-  isDeleted = ATTRIBUTE_DEFAULT.isDeleted,
 }
 
 export enum COLUMN {
   id = 'id',
+  userId = 'user_id',
+  accessToken = 'access_token',
+  accessTokenExp = 'access_token_exp',
+  accessTokenIat = 'access_token_iat',
+  refreshToken = 'refresh_token',
+  refreshTokenExp = 'refresh_token_exp',
+  refreshTokenIat = 'refresh_token_iat',
+  scope = 'scope',
+  tokenType = 'token_type',
+  xeroTokenType = 'xero_token_type',
+  xeroIdToken = 'xero_id_token',
+  xeroSessionState = 'xero_session_state',
+  websiteId = 'website_id',
+  taskId = 'task_id',
+  agentId = 'agent_id',
+  companyMemberId = 'company_member_id',
+  fileTemplateId = 'file_template_id',
+  belongToCompanyMemberId = 'belong_to_company_member_id',
+  ipAddress = 'ip_address',
+  countRequest = 'count_request',
   createdTime = COLUMN_DEFAULT.createdTime,
   updatedTime = COLUMN_DEFAULT.updatedTime,
-  deletedTime = COLUMN_DEFAULT.deletedTime,
-  isDeleted = COLUMN_DEFAULT.isDeleted,
 }
 
 export enum RELATION {
-
+  agent = RELATION_DEFAULT.agent,
+  fileTemplate = RELATION_DEFAULT.fileTemplate,
+  task = RELATION_DEFAULT.task,
+  user = RELATION_DEFAULT.user,
+  website = RELATION_DEFAULT.website,
 }
 
 export enum INDEX {
-
+  agentId = `@@index([${ATTRIBUTE.agentId}], map: "fk_tok_agent_id_age_id_2cha")`,
+  belongToCompanyMemberId = `@@index([${ATTRIBUTE.belongToCompanyMemberId}], map: "fk_tok_belong_to_company_member_id_com_id_9mf5")`,
+  companyMemberId = `@@index([${ATTRIBUTE.companyMemberId}], map: "fk_tok_company_member_id_com_id_lys")`,
+  fileTemplateId = `@@index([${ATTRIBUTE.fileTemplateId}], map: "fk_tok_file_template_id_fil_id_g0lx")`,
+  taskId = `@@index([${ATTRIBUTE.taskId}], map: "fk_tok_task_id_tas_id_g7px")`,
+  userId = `@@index([${ATTRIBUTE.userId}], map: "fk_tok_user_id_use_id_bunm")`,
+  websiteId = `@@index([${ATTRIBUTE.websiteId}], map: "fk_tok_website_id_web_id_4k48")`,
+  accessToken = `@@index([${ATTRIBUTE.accessToken}])`,
+  refreshToken = `@@index([${ATTRIBUTE.refreshToken}])`,
 }
 
 export default {
   ATTRIBUTE,
   RELATION,
 };
-
