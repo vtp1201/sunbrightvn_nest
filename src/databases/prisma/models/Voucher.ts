688a4c972b999133c5b8730931950952f99c71a7
thang.pham <vtp1201@gmail.com>
2024-06-19 01:41:24 +0700

    init openDate

--- src/databases/prisma/models/Voucher.ts
+++ src/databases/prisma/models/Voucher.ts
@@ -1,52 +1,103 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
+import { MODEL_NAME, RAW_DATE_TIME, RAW_STRING, TABLE_NAME } from '../utils';
 import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/Voucher';
 import { createdTime, deleted, updatedTime } from '../mixins';
 
-export default createModel(
-  MODEL_NAME.VOUCHER,
-  (VoucherModel) => {
-    const initCreatedTime = createdTime({
-      attribute: ATTRIBUTE.createdTime,
-      column: COLUMN.createdTime,
-    });
-    const initUpdatedTime = updatedTime({
-      attribute: ATTRIBUTE.updatedTime,
-      column: COLUMN.updatedTime,
-    });
-    const initDeleted = deleted(
-      {
-        attribute: ATTRIBUTE.deletedTime,
-        column: COLUMN.deletedTime,
-      },
-      {
-        attribute: ATTRIBUTE.isDeleted,
-        column: COLUMN.isDeleted,
-      },
-    );
+export default createModel(MODEL_NAME.VOUCHER, (VoucherModel) => {
+  const initCreatedTime = createdTime({
+    attribute: ATTRIBUTE.createdTime,
+    column: COLUMN.createdTime,
+  });
+  const initUpdatedTime = updatedTime({
+    attribute: ATTRIBUTE.updatedTime,
+    column: COLUMN.updatedTime,
+  });
+  const initDeleted = deleted(
+    {
+      attribute: ATTRIBUTE.deletedTime,
+      column: COLUMN.deletedTime,
+    },
+    {
+      attribute: ATTRIBUTE.isDeleted,
+      column: COLUMN.isDeleted,
+    },
+  );
 
-    // defined Model
-    process.nextTick(() => {
-      VoucherModel.int(ATTRIBUTE.id, {
-        id: true,
-        map: COLUMN.id,
-        default: {
-          autoincrement: true,
-        },
+  // defined Model
+  process.nextTick(() => {
+    VoucherModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .string(ATTRIBUTE.name, {
+        map: COLUMN.name,
+        raw: RAW_STRING.LENGTH_255,
+        optional: true,
+      })
+      .string(ATTRIBUTE.voucherCode, {
+        map: COLUMN.voucherCode,
+        raw: RAW_STRING.LENGTH_255,
+      })
+      .int(ATTRIBUTE.customerId, {
+        map: COLUMN.customerId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.googleClientId, {
+        map: COLUMN.googleClientId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.voucherTypeId, {
+        map: COLUMN.voucherTypeId,
+        optional: true,
+      })
+      .int(ATTRIBUTE.orderId, {
+        map: COLUMN.orderId,
+        optional: true,
+      })
+      .boolean(ATTRIBUTE.isActive, {
+        map: COLUMN.isActive,
+        default: false,
+      })
+      .boolean(ATTRIBUTE.isUsed, {
+        map: COLUMN.isUsed,
+        default: false,
+      })
+      .boolean(ATTRIBUTE.isRunning, {
+        map: COLUMN.isRunning,
+        default: false,
+      })
+      .dateTime(ATTRIBUTE.expiredTime, {
+        map: COLUMN.expiredTime,
+        raw: RAW_DATE_TIME.DATE_CURRENT,
+      })
+      .dateTime(ATTRIBUTE.activeDate, {
+        map: COLUMN.activeDate,
+        raw: RAW_DATE_TIME.DATE_CURRENT,
+      })
+      .dateTime(ATTRIBUTE.usedDate, {
+        map: COLUMN.usedDate,
+        raw: RAW_DATE_TIME.DATE_CURRENT,
+      })
+      .dateTime(ATTRIBUTE.openDate, {
+        map: COLUMN.openDate,
+        raw: RAW_DATE_TIME.DATE_CURRENT,
       })
 
-        // dateTime marks
-        .mixin(initCreatedTime)
-        .mixin(initUpdatedTime)
-        .mixin(initDeleted)
+      // dateTime marks
+      .mixin(initCreatedTime)
+      .mixin(initUpdatedTime)
+      .mixin(initDeleted)
 
-        // indexes
-        .raw()
+      // indexes
+      .raw(INDEX.customerId)
+      .raw(INDEX.orderId)
+      .raw(INDEX.voucherTypeId)
 
-        // table name
-        .map(TABLE_NAME.VOUCHER);
-    });
-  },
-);
-
+      // table name
+      .map(TABLE_NAME.VOUCHER);
+  });
+});
--- src/databases/prisma/utils/enums/Voucher.ts
+++ src/databases/prisma/utils/enums/Voucher.ts
@@ -1,7 +1,20 @@
-import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT } from './default';
+import { ATTRIBUTE_DEFAULT, COLUMN_DEFAULT, RELATION_DEFAULT } from './default';
 
 export enum ATTRIBUTE {
   id = 'id',
+  name = 'name',
+  voucherCode = 'voucherCode',
+  customerId = 'customerId',
+  googleClientId = 'googleClientId',
+  voucherTypeId = 'voucherTypeId',
+  orderId = 'orderId',
+  isActive = 'isActive',
+  isUsed = 'isUsed',
+  isRunning = 'isRunning',
+  expiredTime = 'expiredTime',
+  activeDate = 'activeDate',
+  usedDate = 'usedDate',
+  openDate = 'openDate',
   createdTime = ATTRIBUTE_DEFAULT.createdTime,
   updatedTime = ATTRIBUTE_DEFAULT.updatedTime,
   deletedTime = ATTRIBUTE_DEFAULT.deletedTime,
@@ -10,6 +23,19 @@ export enum ATTRIBUTE {
 
 export enum COLUMN {
   id = 'id',
+  name = 'name',
+  voucherCode = 'voucher_code',
+  customerId = 'customer_id',
+  googleClientId = 'google_client_id',
+  voucherTypeId = 'voucher_type_id',
+  orderId = 'order_id',
+  isActive = 'is_active',
+  isUsed = 'is_used',
+  isRunning = 'is_running',
+  expiredTime = 'expired_time',
+  activeDate = 'active_date',
+  usedDate = 'used_date',
+  openDate = 'open_date',
   createdTime = COLUMN_DEFAULT.createdTime,
   updatedTime = COLUMN_DEFAULT.updatedTime,
   deletedTime = COLUMN_DEFAULT.deletedTime,
@@ -17,15 +43,18 @@ export enum COLUMN {
 }
 
 export enum RELATION {
-
+  customer = RELATION_DEFAULT.customer,
+  order = RELATION_DEFAULT.order,
+  voucherType = RELATION_DEFAULT.voucherType,
 }
 
 export enum INDEX {
-
+  customerId = `@@index([${ATTRIBUTE.customerId}], map: "fk_vou_customer_id_cus_id_7f7y")`,
+  orderId = `@@index([${ATTRIBUTE.orderId}], map: "fk_vou_order_id_ord_id_8rjw")`,
+  voucherTypeId = `@@index([${ATTRIBUTE.voucherTypeId}], map: "fk_vou_voucher_type_id_vou_id_bgsy")`,
 }
 
 export default {
   ATTRIBUTE,
   RELATION,
 };
-
