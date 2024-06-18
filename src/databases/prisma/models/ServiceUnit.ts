3d175e263bf760d28604a461ada775cfda2f3336
Pham Viet Thang <thangpham@gleadsvn.local>
2024-06-18 13:08:24 +0700

    init ServiceUnitModel

--- src/databases/prisma/models/ServiceUnit.ts
+++ src/databases/prisma/models/ServiceUnit.ts
@@ -1,52 +1,44 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
-import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/ServiceUnit';
-import { createdTime, deleted, updatedTime } from '../mixins';
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/ServiceUnit';
+import { deleted } from '../mixins';
 
-export default createModel(
-  MODEL_NAME.SERVICE_UNIT,
-  (ServiceUnitModel) => {
-    const initCreatedTime = createdTime({
-      attribute: ATTRIBUTE.createdTime,
-      column: COLUMN.createdTime,
-    });
-    const initUpdatedTime = updatedTime({
-      attribute: ATTRIBUTE.updatedTime,
-      column: COLUMN.updatedTime,
-    });
-    const initDeleted = deleted(
-      {
-        attribute: ATTRIBUTE.deletedTime,
-        column: COLUMN.deletedTime,
-      },
-      {
-        attribute: ATTRIBUTE.isDeleted,
-        column: COLUMN.isDeleted,
-      },
-    );
+export default createModel(MODEL_NAME.SERVICE_UNIT, (ServiceUnitModel) => {
+  const initDeleted = deleted(
+    {
+      attribute: ATTRIBUTE.deletedTime,
+      column: COLUMN.deletedTime,
+    },
+    {
+      attribute: ATTRIBUTE.isDeleted,
+      column: COLUMN.isDeleted,
+    },
+  );
 
-    // defined Model
-    process.nextTick(() => {
-      ServiceUnitModel.int(ATTRIBUTE.id, {
-        id: true,
-        map: COLUMN.id,
-        default: {
-          autoincrement: true,
-        },
+  // defined Model
+  process.nextTick(() => {
+    ServiceUnitModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .string(ATTRIBUTE.name, {
+        map: COLUMN.name,
+        raw: RAW_STRING.LENGTH_45,
+      })
+      .string(ATTRIBUTE.plural, {
+        map: COLUMN.plural,
+        raw: RAW_STRING.LENGTH_45,
+        optional: true,
       })
 
-        // dateTime marks
-        .mixin(initCreatedTime)
-        .mixin(initUpdatedTime)
-        .mixin(initDeleted)
+      // dateTime marks
+      .mixin(initDeleted)
 
-        // indexes
-        .raw()
-
-        // table name
-        .map(TABLE_NAME.SERVICE_UNIT);
-    });
-  },
-);
-
+      // table name
+      .map(TABLE_NAME.SERVICE_UNIT);
+  });
+});
