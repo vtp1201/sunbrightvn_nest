a642a86b33491da891cbda3512bf459aafeb483f
thang.pham <vtp1201@gmail.com>
2024-06-19 00:45:23 +0700

    init UrlListTypeModel

--- src/databases/prisma/models/UrlListType.ts
+++ src/databases/prisma/models/UrlListType.ts
@@ -1,52 +1,39 @@
 import { createModel } from 'schemix';
 
-import { MODEL_NAME, TABLE_NAME } from '../utils';
-import { ATTRIBUTE, COLUMN, INDEX } from '../utils/enums/UrlListType';
-import { createdTime, deleted, updatedTime } from '../mixins';
+import { MODEL_NAME, RAW_STRING, TABLE_NAME } from '../utils';
+import { ATTRIBUTE, COLUMN } from '../utils/enums/UrlListType';
+import { deleted } from '../mixins';
 
-export default createModel(
-  MODEL_NAME.URL_LIST_TYPE,
-  (UrlListTypeModel) => {
-    const initCreatedTime = createdTime({
-      attribute: ATTRIBUTE.createdTime,
-      column: COLUMN.createdTime,
-    });
-    const initUpdatedTime = updatedTime({
-      attribute: ATTRIBUTE.updatedTime,
-      column: COLUMN.updatedTime,
-    });
-    const initDeleted = deleted(
-      {
-        attribute: ATTRIBUTE.deletedTime,
-        column: COLUMN.deletedTime,
-      },
-      {
-        attribute: ATTRIBUTE.isDeleted,
-        column: COLUMN.isDeleted,
-      },
-    );
+export default createModel(MODEL_NAME.URL_LIST_TYPE, (UrlListTypeModel) => {
+  const initDeleted = deleted(
+    {
+      attribute: ATTRIBUTE.deletedTime,
+      column: COLUMN.deletedTime,
+    },
+    {
+      attribute: ATTRIBUTE.isDeleted,
+      column: COLUMN.isDeleted,
+    },
+  );
 
-    // defined Model
-    process.nextTick(() => {
-      UrlListTypeModel.int(ATTRIBUTE.id, {
-        id: true,
-        map: COLUMN.id,
-        default: {
-          autoincrement: true,
-        },
+  // defined Model
+  process.nextTick(() => {
+    UrlListTypeModel.int(ATTRIBUTE.id, {
+      id: true,
+      map: COLUMN.id,
+      default: {
+        autoincrement: true,
+      },
+    })
+      .string(ATTRIBUTE.name, {
+        map: COLUMN.name,
+        raw: RAW_STRING.LENGTH_45,
       })
 
-        // dateTime marks
-        .mixin(initCreatedTime)
-        .mixin(initUpdatedTime)
-        .mixin(initDeleted)
+      // dateTime marks
+      .mixin(initDeleted)
 
-        // indexes
-        .raw()
-
-        // table name
-        .map(TABLE_NAME.URL_LIST_TYPE);
-    });
-  },
-);
-
+      // table name
+      .map(TABLE_NAME.URL_LIST_TYPE);
+  });
+});
