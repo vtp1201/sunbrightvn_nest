de8beeb1001f14b20911993e9f478a0fe9b20a3d
thang.pham <vtp1201@gmail.com>
2024-06-23 22:35:33 +0700

    add middleware prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ENV_PATH } from '@utilities';
 import { validate } from '@configs/config.validate';
+import { RedisModule } from '@nestjs-modules/ioredis';
 
 @Module({
   imports: [
@@ -9,6 +10,10 @@ import { validate } from '@configs/config.validate';
       envFilePath: ENV_PATH,
       validate,
       isGlobal: true,
+    }),
+    RedisModule.forRoot({
+      type: 'single',
+      url: '',
     }),
   ],
   controllers: [],
