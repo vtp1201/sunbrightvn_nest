9baac1fd4e4f28214d5be23ef30725b54ab4a395
thang.pham <vtp1201@gmail.com>
2024-06-23 17:02:15 +0700

    validate env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ENV_PATH } from '@utilities';
+import { validate } from '@configs/config.validate';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ENV_PATH,
+      validate,
       isGlobal: true,
     }),
   ],
--- src/configs/config.dto.ts
+++ src/configs/config.dto.ts
@@ -1,32 +1,43 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class ConfigEnvDto {
   @IsNotEmpty()
+  @IsString()
   readonly MYSQL_USERNAME: string;
 
+  @IsString()
   @IsNotEmpty()
   readonly MYSQL_PASSWORD: string;
 
+  @IsString()
   @IsNotEmpty()
   readonly MYSQL_DATABASE: string;
 
+  @IsString()
   @IsNotEmpty()
   readonly MYSQL_HOST: string;
+
   @IsNotEmpty()
+  @IsNumber()
   readonly MYSQL_PORT: string;
 
+  @IsString()
   @IsNotEmpty()
   readonly MYSQL_URL: string;
 
+  @IsString()
   @IsNotEmpty()
   readonly MAILCHIMP_API_KEY: string;
 
+  @IsString()
   @IsNotEmpty()
   readonly MAILCHIMP_AUDIENCE_ID: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly MAILCHIMP_URI: string;
 
+  @IsString()
   @IsNotEmpty()
   readonly MAILCHIMP_USERNAME: string;
 }
