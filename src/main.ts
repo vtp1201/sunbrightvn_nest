76f3b939e98c6007637bf1ccc19dccb565581032
Pham Viet Thang <thangpham@gleadsvn.local>
2024-07-11 12:47:02 +0700

    init ejs

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { configSwagger } from '@configs/apiDocs.config';
 import helmet from 'helmet';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
 
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   configSwagger(app);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, 'views'));
+  app.setViewEngine('ejs');
 
   await app.listen(3000);
 }
