b183454593c48f829ff99502c40b38914fc6c2ad
Pham Viet Thang <thangpham@gleadsvn.local>
2024-07-11 13:03:44 +0700

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/views/*.ejs'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  ],
+};
